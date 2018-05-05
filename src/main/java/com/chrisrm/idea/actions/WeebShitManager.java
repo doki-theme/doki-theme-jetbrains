eadb5193ad2cf97843e429d21882a55001bc61fa
DATBOI <alexsimons9999@gmail.com>
2018-05-04 21:03:56 -0500

    It is just monika at the moment

--- src/main/java/com/chrisrm/idea/actions/WeebShitManager.java
+++ src/main/java/com/chrisrm/idea/actions/WeebShitManager.java
@@ -3,9 +3,12 @@ package com.chrisrm.idea.actions;
 import com.chrisrm.idea.MTThemes;
 import com.intellij.ide.util.PropertiesComponent;
 import com.intellij.openapi.project.Project;
+import com.intellij.openapi.wm.impl.IdeBackgroundUtil;
 
 import java.util.Optional;
 import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import static com.intellij.openapi.wm.impl.IdeBackgroundUtil.EDITOR_PROP;
 
@@ -15,6 +18,7 @@ public final class WeebShitManager {
   private static final String WEEB_SHIT_PROPERTY = "WEEB_SHIT_PROPERTY";
   private final AtomicBoolean isOn = new AtomicBoolean(false);
   private Optional<Project> projectRef = Optional.empty();
+  private MTThemes mtThemes = MTThemes.MONIKA;
 
   private WeebShitManager() {
   }
@@ -55,12 +59,35 @@ public final class WeebShitManager {
   }
 
   private void turnOnWeebShit(Project project) {
-    PropertiesComponent.getInstance().setValue(EDITOR_PROP, "");
+    String imagePath = getImagePath();
+    String opacity = "75";
+    String fill = "plain";//ref -> IdeBackgroundUtil.Fill.PLAIN
+    String anchor = "bottom_right";//ref -> IdeBackgroundUtil.Anchor.BOTTOM_RIGHT;
+    String property = Stream.of(imagePath, opacity, fill, anchor)
+        .collect(Collectors.joining(","));
+    PropertiesComponent.getInstance().setValue(EDITOR_PROP, property);
+  }
+
+  private String getImagePath() {
+    return "/webstuff/" + getTheme()
+        .map(theme -> {
+          switch (theme){
+            default:
+            case MONIKA:
+              return "just_monika.png";
+          }
+        })
+        .orElse("just_monika.png");
   }
 
   private void removeWeebShit(Project project) {
     PropertiesComponent.getInstance().setValue(EDITOR_PROP, null);
   }
+
+  private Optional<MTThemes> getTheme(){
+    return Optional.ofNullable(this.mtThemes);
+  }
+
 
   public void activate(MTThemes monika) {
 
