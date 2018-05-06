58fdb7e0297c9c0bd17b1442a5a892f0b0fd2864
DATBOI <alexsimons9999@gmail.com>
2018-05-05 20:07:31 -0500

    Start of new sayori colors.

--- src/main/java/com/chrisrm/idea/themes/literature/club/MTSayoriTheme.java
+++ src/main/java/com/chrisrm/idea/themes/literature/club/MTSayoriTheme.java
@@ -4,4 +4,24 @@ public final class MTSayoriTheme extends MTDokiDokiTheme {
   public MTSayoriTheme() {
     super("mt.sayori", "Material Monika", false);
   }
+
+  @Override
+  protected String getBackgroundColorString() {
+    return "fbfcfc";
+  }
+
+  @Override
+  protected String getSecondaryBackgroundColorString() {
+    return "c7f2ff";
+  }
+
+  @Override
+  protected String getSecondaryForegroundColorString() {
+    return "256fe2";
+  }
+
+  @Override
+  protected String getSelectionForegroundColorString() {
+    return "256fe2";
+  }
 }
