40bf5f003b140b818b100a42059f4017cac28d58
DATBOI <alexsimons9999@gmail.com>
2018-05-12 14:01:11 -0500

    Purple has a lot of dark shades, maybe I can make dark theme for all of the club members?

--- src/main/java/com/chrisrm/idea/themes/literature/club/MTYuriTheme.java
+++ src/main/java/com/chrisrm/idea/themes/literature/club/MTYuriTheme.java
@@ -4,4 +4,59 @@ public final class MTYuriTheme extends MTDokiDokiTheme {
   public MTYuriTheme() {
     super("mt.yuri", "Material Yuri", false);
   }
+
+  @Override
+  protected String getBackgroundColorString() {
+    return "eaf0fc";
+  }
+
+  @Override
+  protected String getSecondaryBackgroundColorString() {
+    return "bbb8ff";
+  }
+
+  @Override
+  protected String getSecondaryForegroundColorString() {
+    return "b16ffa";
+  }
+
+  @Override
+  protected String getSelectionForegroundColorString() {
+    return "c06fff";
+  }
+
+  @Override
+  protected String getSelectionBackgroundColorString() {
+    return "e4caff";
+  }
+
+  @Override
+  protected String getTreeSelectionBackgroundColorString() {
+    return "d3a5fa";
+  }
+
+  @Override
+  protected String getMenuBarSelectionForegroundColorString() {
+    return "ffffff";
+  }
+
+  @Override
+  protected String getMenuBarSelectionBackgroundColorString() {
+    return "7c0e9d";
+  }
+
+  @Override
+  protected String getNotificationsColorString() {
+    return "d3ceff";
+  }
+
+  @Override
+  protected String getContrastColorString() {
+    return "ccaaff";
+  }
+
+  @Override
+  protected String getEditorTabColorString() {
+    return getContrastColorString();
+  }
 }
