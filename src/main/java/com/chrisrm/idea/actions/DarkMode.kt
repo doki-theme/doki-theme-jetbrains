5a4812e113bc2e46967825e4c7512bc18b597223
DATBOI <alexsimons9999@gmail.com>
2018-06-30 17:53:45 -0500

    Persisted dark mode being on or off

--- src/main/java/com/chrisrm/idea/actions/DarkMode.kt
+++ src/main/java/com/chrisrm/idea/actions/DarkMode.kt
@@ -1,5 +1,7 @@
 package com.chrisrm.idea.actions
 
+import com.intellij.ide.util.PropertiesComponent
+
 /**
  * Forged in the flames of battle by alex.
  */
@@ -7,13 +9,21 @@ package com.chrisrm.idea.actions
 class DarkMode {
 
     companion object {
+
         private var isOn = false
+        private val SAVED_STATE = "DARK_MODE_ON"
+
+        init {
+            isOn = PropertiesComponent.getInstance().getValue(SAVED_STATE)?.toBoolean() ?: false
+        }
 
         @JvmStatic
         fun isOn() = isOn
 
         fun toggle() {
             isOn = isOn.not()
+            PropertiesComponent.getInstance()
+                    .setValue(SAVED_STATE, isOn)
         }
     }
 }
