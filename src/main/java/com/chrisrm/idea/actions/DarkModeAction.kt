cd61189fb3a690e9b61aebd4bcf69cbadc521dda
DATBOI <alexsimons9999@gmail.com>
2018-07-04 12:10:40 -0500

    ṃ̴̛͓̩͑̌͆̊̀̏̀̈́͐̃̽̔͘͠o̴̙̅͋r̵̭̓́̈́̂̓̂͂e̸̟͇̝̕͠ ̵̻̠̰̑͋͗̅͑̆́͊̓̄̾̿͐͘̕s̵̤̼̟̻̟̖̖̤̬̣̭͛͐̀ä̷̡̡̱͇́͝y̶̡̡̢͉̱͓̜̩͍̩͚̪̫̹̑̍̃̆͛̾̏͆̍̔̏͛͒̚͠ǫ̸̎̽̓̍r̵̨͍̣̮͈̹̲͕̬̠̱̜̿̈i̴̧̡̡̗̣̙̗͖̜̩̙͉͋͛̀̅̋̈̎̐̀͜ͅ ̴̛̜̖͙͎̤̩̯̩̉̀̔́̅̓̿̽͑̄͐̔̚͝d̷͙̬͑̈́̌̏͆̌̊͑̇́̃̋͝͠ȁ̵̖̞̩̗͛̂͛̋̾̈́̒͐̈́̒͐͊̚͜ř̵̛̳̹̞̮͓̦͉̠̰̻̼͍͎̗̻͛͐̐̉̔̌̂̀͘̕k̶̡̨̨͎͕̥͍̙͔͙̳͑̉͌ ̷̧̻̰̪̙͚̗̬̈́͊̅̎̐͊̊̓̋͑̓͗̃̕͝m̷̧̨̨̥̒̈́̀͋̚̚͠o̷̗̓̆̎̇̽̀̿̌̌͛̏̈́̔̕͘d̴̨̩͇̦͎̦̻̝̂̇̒͋̌̎̐͜͜e̸͍͉̪͖͇͍͇̘͍͉͇̰̲͌́̈́̉̽́̊̂̓̚ͅ ̶̦̗͕̼͎̭̲͎̬̮̀̇̇́̓̊́̓̀͂͜͝t̴̨̧̹͓͚̳̞̤̗̭̼̭̜̃o̸̝̣̦̙͕̥̞̗̱͕̪̣͛̍̈́͗̎̂̃͛͝ģ̷̣̅̃̒͆̈́̈́̌̎̂͘̕g̵̢̝̟̪̺͕̓̆̾̈́͛́̔͝͝l̴͈͙̳̮̲̞͖̞͛e̶͍͚͔̦̓̉͗͝͠ͅ

--- src/main/java/com/chrisrm/idea/actions/DarkModeAction.kt
+++ src/main/java/com/chrisrm/idea/actions/DarkModeAction.kt
@@ -3,6 +3,7 @@ package com.chrisrm.idea.actions
 import com.chrisrm.idea.MTConfig
 import com.chrisrm.idea.MTThemes
 import com.chrisrm.idea.actions.themes.literature.club.JustMonikaThemeAction
+import com.chrisrm.idea.actions.themes.literature.club.SayoriThemeAction
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.actionSystem.ToggleAction
 
@@ -17,7 +18,7 @@ class DarkModeAction : ToggleAction("SPOILERS!!!") {
         DarkMode.toggle()
         when (MTConfig.getInstance().getSelectedTheme()) {
             MTThemes.MONIKA -> JustMonikaThemeAction.setSelected(e, state)
-//            MTThemes.SAYORI -> SayoriThemeAction.setSelected(e, state)
+            MTThemes.SAYORI -> SayoriThemeAction.setSelected(e, state)
 //            MTThemes.NATSUKI -> NatsukiThemeAction.setSelected(e, state)
 //            MTThemes.YURI -> YuriThemeAction.setSelected(e, state)
         }
