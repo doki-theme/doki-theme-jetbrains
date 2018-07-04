5f235ed84351bb95d2e71f9a87c31dac309782bd
DATBOI <alexsimons9999@gmail.com>
2018-07-04 12:23:18 -0500

    Natsuki theme action

--- src/main/java/com/chrisrm/idea/actions/themes/literature/club/NatsukiThemeAction.kt
+++ src/main/java/com/chrisrm/idea/actions/themes/literature/club/NatsukiThemeAction.kt
@@ -24,28 +24,30 @@
  *
  */
 
-package com.chrisrm.idea.actions.themes.literature.club;
+package com.chrisrm.idea.actions.themes.literature.club
 
-import com.chrisrm.idea.MTConfig;
-import com.chrisrm.idea.MTThemeManager;
-import com.chrisrm.idea.MTThemes;
-import com.chrisrm.idea.actions.ClubMemberManager;
-import com.chrisrm.idea.actions.accents.MTFuschiaAccentAction;
-import com.chrisrm.idea.actions.themes.MTBaseThemeAction;
-import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.chrisrm.idea.MTConfig
+import com.chrisrm.idea.MTThemeManager
+import com.chrisrm.idea.MTThemes
+import com.chrisrm.idea.actions.ClubMemberManager
+import com.chrisrm.idea.actions.accents.MTFuschiaAccentAction
+import com.chrisrm.idea.actions.themes.MTBaseThemeAction
+import com.chrisrm.idea.actions.themes.ToggleThemeAction
+import com.intellij.openapi.actionSystem.AnActionEvent
 
-public final class MTNatsukiThemeAction extends MTBaseThemeAction {
-  private final MTFuschiaAccentAction mtFuschiaAccentAction = new MTFuschiaAccentAction();
-  @Override
-  public boolean isSelected(final AnActionEvent e) {
-    return MTConfig.getInstance().getSelectedTheme() == MTThemes.NATSUKI;
-  }
+class NatsukiThemeAction : MTBaseThemeAction() {
+    override fun isSelected(e: AnActionEvent): Boolean =
+            MTConfig.getInstance().getSelectedTheme() === MTThemes.NATSUKI
 
-  @Override
-  public void setSelected(final AnActionEvent e, final boolean state) {
-    super.setSelected(e, state);
-    mtFuschiaAccentAction.actionPerformed(e);
-    MTThemeManager.getInstance().activate(MTThemes.NATSUKI, true);
-    ClubMemberManager.getInstance().activate(MTThemes.NATSUKI);
-  }
+    override fun setSelected(e: AnActionEvent, state: Boolean) =
+            NatsukiThemeAction.setSelected(e, state)
+
+    companion object : ToggleThemeAction {
+        private val mtFuschiaAccentAction = MTFuschiaAccentAction()
+        override fun setSelected(e: AnActionEvent, state: Boolean) {
+            mtFuschiaAccentAction.actionPerformed(e)
+            MTThemeManager.getInstance().activate(MTThemes.NATSUKI, true)
+            ClubMemberManager.getInstance().activate(MTThemes.NATSUKI)
+        }
+    }
 }
