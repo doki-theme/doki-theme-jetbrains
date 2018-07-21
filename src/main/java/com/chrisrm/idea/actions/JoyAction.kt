dfbd075253eecc8c41f4c88db638c90a174ef854
DATBOI <alexsimons9999@gmail.com>
2018-07-21 18:43:03 -0500

    So they change the toggle action in 2018.2

--- src/main/java/com/chrisrm/idea/actions/JoyAction.kt
+++ src/main/java/com/chrisrm/idea/actions/JoyAction.kt
@@ -2,13 +2,16 @@ package com.chrisrm.idea.actions
 
 import com.chrisrm.idea.MTConfig
 import com.chrisrm.idea.MTThemes
+import com.chrisrm.idea.actions.themes.MTAbstractThemeAction
+import com.chrisrm.idea.utils.IconCache
+import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.actionSystem.ToggleAction
 
 /**
  * Forged in the flames of battle by alex.
  */
-class JoyAction: ToggleAction("Some Joy") {
+class JoyAction: MTAbstractThemeAction() {
     override fun isSelected(e: AnActionEvent?) = JoyManager.isOn()
 
     // todo: will need to integrate with the new stuff on master.
