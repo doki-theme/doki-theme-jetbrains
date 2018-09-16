a26ac413233956146f0c963d0ce23f88fda261eb
DATBOI <alexsimons9999@gmail.com>
2018-09-16 13:59:32 -0500

    just going to take a shot in the dark with dark natsuki

--- src/main/java/com/chrisrm/idea/themes/literature/club/dark/OnlyPlayWithMeTheme.kt
+++ src/main/java/com/chrisrm/idea/themes/literature/club/dark/OnlyPlayWithMeTheme.kt
@@ -1,8 +1,9 @@
 package com.chrisrm.idea.themes.literature.club.dark
 
 import com.chrisrm.idea.themes.literature.club.DokiDokiTheme
+import java.util.stream.Stream
 
-class OnlyPlayWithMeTheme: DokiDokiTheme("natsuki.dark", "Only Play With Me", true, "Natsuki") {
+class OnlyPlayWithMeTheme : DokiDokiTheme("natsuki.dark", "Only Play With Me", true, "Natsuki") {
 
     override fun getBackgroundColorString(): String = "240E1E"
 
@@ -45,4 +46,59 @@ class OnlyPlayWithMeTheme: DokiDokiTheme("natsuki.dark", "Only Play With Me", tr
     override fun getSecondBorderColorString(): String = "5A1943"
 
     override fun getDisabledColorString(): String = "232323"
+
+    override fun getPropertyStream(): Stream<Pair<String, String>> {
+        return Stream.of(
+                Pair("natsuki.dark.background", "231921"),
+                Pair("Panel.background", "2B2827"),
+                Pair("Menu.foreground", "ffffff"),
+                Pair("PopupMenu.background", "874C57"),
+                Pair("Menu.background", "874C57"),
+                Pair("MenuBar.background", "874C57"),
+                Pair("Menu.acceleratorForeground", "ffffff"),
+                Pair("MenuBar.foreground", "ffffff"),
+                Pair("Button.mt.foreground", "ff6fd5"),
+                Pair("MenuItem.selectionForeground", "ffffff"),
+                Pair("MenuItem.selectionBackground", "AA041A"),
+                Pair("MenuItem.foreground", "FFFFFF"),
+                Pair("Menu.selectionForeground", "ffffff"),
+                Pair("Menu.selectionBackground", "AA041A"),
+                Pair("Notifications.background", "B17FB1"),
+                Pair("Notifications.borderColor", "ffc4ff"),
+                Pair("Table.selectionForeground", "000000"),
+                Pair("Table.selectionBackground", "CB7BCA"),
+                Pair("Table.highlightOuter", "FFD3F6"),
+                Pair("Autocomplete.selectionBackground", "462325"),
+                Pair("Autocomplete.selectionForeground", "FB67B7"),
+                Pair("Autocomplete.selectionForegroundGreyed", "FFFFFF"),
+                Pair("Autocomplete.background", "401925"),
+                Pair("Autocomplete.foreground", "FC99D0"),
+                Pair("Autocomplete.selectedGreyedForeground", "F844D2"),
+                Pair("Autocomplete.prefixForeground", "F4B1F8"),
+                Pair("Autocomplete.selectedPrefixForeground", "FFEAFD"),
+                Pair("Autocomplete.selectionUnfocus", "542A30"),
+                Pair("Button.mt.color2", "9B3685"),
+                Pair("Button.mt.primary.color", "7E436E"),
+                Pair("Button.mt.selection.color1", "B9589F"),
+                Pair("Button.mt.selection.color2", "B3B2B2"),
+                Pair("Button.mt.background", "9B3685"),
+                Pair("Button.mt.selectedForeground", "FFFFFF"),
+                Pair("Button.mt.color1", "9B3685"),
+                Pair("ToolBar.background", "9B3685"),
+                Pair("EditorPane.caretForeground", "9B3685"),
+                Pair("SearchEverywhere.background", "CC91C9"),
+                Pair("SearchEverywhere.foreground", "AE36B6"),
+                Pair("SearchEverywhere.shortcutForeground", "C1B7C5"),
+                Pair("natsuki.dark.textBackground", "4A1D3B"),
+                Pair("natsuki.dark.foreground", "A76E9F"),
+                Pair("natsuki.dark.textForeground", "A76E9F"),
+                Pair("natsuki.dark.caretForeground", "FFCC00"),
+                Pair("natsuki.dark.inactiveBackground", "4A1D3B"),
+                Pair("natsuki.dark.selectionForeground", "FFFFFF"),
+                Pair("natsuki.dark.selectionBackgroundInactive", "D2D4D5"),
+                Pair("natsuki.dark.selectionInactiveBackground", "D2D4D5"),
+                Pair("natsuki.dark.selectionForegroundInactive", "A76E9F"),
+                Pair("natsuki.dark.selectionInactiveForeground", "A76E9F")
+        )
+    }
 }
