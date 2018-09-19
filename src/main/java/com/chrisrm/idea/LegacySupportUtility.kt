cfb9ee329618606829de6ef7d4b2cab5d72e5d71
DATBOI <alexsimons9999@gmail.com>
2018-09-19 18:33:58 -0500

    One down, probably more to go

--- src/main/java/com/chrisrm/idea/LegacySupportUtility.kt
+++ src/main/java/com/chrisrm/idea/LegacySupportUtility.kt
@@ -1,18 +1,22 @@
 package com.chrisrm.idea
 
+import com.chrisrm.idea.legacy.Runner
+
 object LegacySupportUtility {
 
-    fun invokeClassSafely(clasName: String, runSafely: ()-> Void) {
-        try{
+    fun invokeClassSafely(clasName: String, runSafely: () -> Void) {
+        try {
             Class.forName(clasName)
             runSafely()
-        } catch (ignored: ClassNotFoundException){}
+        } catch (ignored: ClassNotFoundException) {
+        }
     }
 
-    fun invokeClassSafely(clasName: String, runSafely: Runnable) {
-        try{
+    fun invokeClassSafely(clasName: String, runSafely: Runner) {
+        try {
             Class.forName(clasName)
             runSafely.run()// :|
-        } catch (ignored: ClassNotFoundException){}
+        } catch (ignored: Exception) {
+        }
     }
 }
