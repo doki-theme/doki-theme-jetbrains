819ed5c5a1941e243b17cf81dbc65661c6ea35e8
DATBOI <alexsimons9999@gmail.com>
2018-09-24 19:00:08 -0500

    I now what I need to do, just need to figure out how to do it

--- src/main/java/com/chrisrm/idea/DDLCHackComponent.kt
+++ src/main/java/com/chrisrm/idea/DDLCHackComponent.kt
@@ -2,11 +2,10 @@ package com.chrisrm.idea
 
 import com.intellij.ide.util.TipPanel
 import com.intellij.openapi.components.ApplicationComponent
-import javassist.CannotCompileException
-import javassist.ClassClassPath
-import javassist.ClassPool
 import javassist.expr.ExprEditor
 import javassist.expr.MethodCall
+import javassist.*
+
 
 object DDLCHackComponent : ApplicationComponent {
 
@@ -15,6 +14,11 @@ object DDLCHackComponent : ApplicationComponent {
     }
 
     private fun createMonikasWritingTipOfTheDay() {
+        hackDialog()
+        hackMenu()
+    }
+
+    private fun hackDialog() {
         try {
             val cp = ClassPool(true)
             cp.insertClassPath(ClassClassPath(TipPanel::class.java))
@@ -33,6 +37,26 @@ object DDLCHackComponent : ApplicationComponent {
         } catch (e: Exception) {
             e.printStackTrace()
         }
+    }
 
+    private fun hackMenu() {
+        try {
+            val cp = ClassPool.getDefault()
+            cp.insertClassPath("/com/intellij/idea/")
+            val ctClass = cp.get("com.intellij.idea.ActionsBundle")
+
+            val init = ctClass.getDeclaredMethod("initialize")
+            init.instrument(object : ExprEditor() {
+                @Throws(CannotCompileException::class)
+                override fun edit(m: MethodCall?) {
+                    if (m!!.methodName == "message") {
+                        m.replace("{ \$_ = \"Monika's Writing Tip of the Day\"; }")
+                    }
+                }
+            })
+            ctClass.toClass()
+        } catch (e: Throwable) {
+            e.printStackTrace()
+        }
     }
 }
