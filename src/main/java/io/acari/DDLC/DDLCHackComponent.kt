00cc2943c4a1797b9b6513d59408a18f7b69d35e
DATBOI <alexsimons9999@gmail.com>
2018-09-25 18:34:49 -0500

    So close

--- src/main/java/io/acari/DDLC/DDLCHackComponent.kt
+++ src/main/java/io/acari/DDLC/DDLCHackComponent.kt
@@ -1,10 +1,12 @@
-package com.chrisrm.idea
+package io.acari.DDLC
 
 import com.intellij.ide.util.TipPanel
+import com.intellij.idea.IdeaTestApplication
 import com.intellij.openapi.components.ApplicationComponent
 import javassist.expr.ExprEditor
 import javassist.expr.MethodCall
 import javassist.*
+import java.util.*
 
 
 object DDLCHackComponent : ApplicationComponent {
@@ -41,16 +43,16 @@ object DDLCHackComponent : ApplicationComponent {
 
     private fun hackMenu() {
         try {
-            val cp = ClassPool.getDefault()
-            cp.insertClassPath("/com/intellij/idea/")
+            val cp = ClassPool(true)
+            cp.insertClassPath(ClassClassPath(IdeaTestApplication::class.java))
             val ctClass = cp.get("com.intellij.idea.ActionsBundle")
-
-            val init = ctClass.getDeclaredMethod("initialize")
+            val bundle = java.util.ResourceBundle.getBundle("io.acari.DDLC.DDLCActionsBundle", Locale.US)
+            val init = ctClass.getDeclaredMethod("getBundle")
             init.instrument(object : ExprEditor() {
                 @Throws(CannotCompileException::class)
                 override fun edit(m: MethodCall?) {
-                    if (m!!.methodName == "message") {
-                        m.replace("{ \$_ = \"Monika's Writing Tip of the Day\"; }")
+                    if (m!!.methodName == "getBundle") {
+                        m.replace("{ \$_ = java.util.ResourceBundle.getBundle(\"io.acari.DDLC.DDLCActionsBundle\", java.util.Locale.US); }")
                     }
                 }
             })
