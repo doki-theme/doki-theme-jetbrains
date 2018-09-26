17be08abf323c5d3600dda3b2635c1fbb4d4975e
DATBOI <alexsimons9999@gmail.com>
2018-09-26 18:43:38 -0500

    Not going to do that for now

--- src/main/java/io/acari/DDLC/DDLCHackComponent.kt
+++ src/main/java/io/acari/DDLC/DDLCHackComponent.kt
@@ -6,7 +6,6 @@ import com.intellij.openapi.components.ApplicationComponent
 import javassist.expr.ExprEditor
 import javassist.expr.MethodCall
 import javassist.*
-import java.util.*
 
 
 object DDLCHackComponent : ApplicationComponent {
@@ -17,7 +16,6 @@ object DDLCHackComponent : ApplicationComponent {
 
     private fun createMonikasWritingTipOfTheDay() {
         hackDialog()
-        hackMenu()
     }
 
     private fun hackDialog() {
@@ -37,27 +35,6 @@ object DDLCHackComponent : ApplicationComponent {
             })
             ctClass.toClass()
         } catch (e: Exception) {
-            e.printStackTrace()
-        }
-    }
-
-    private fun hackMenu() {
-        try {
-            val cp = ClassPool(true)
-            cp.insertClassPath(ClassClassPath(IdeaTestApplication::class.java))
-            val ctClass = cp.get("com.intellij.idea.ActionsBundle")
-            val bundle = java.util.ResourceBundle.getBundle("io.acari.DDLC.DDLCActionsBundle", Locale.US)
-            val init = ctClass.getDeclaredMethod("getBundle")
-            init.instrument(object : ExprEditor() {
-                @Throws(CannotCompileException::class)
-                override fun edit(m: MethodCall?) {
-                    if (m!!.methodName == "getBundle") {
-                        m.replace("{ \$_ = java.util.ResourceBundle.getBundle(\"io.acari.DDLC.DDLCActionsBundle\", java.util.Locale.US); }")
-                    }
-                }
-            })
-            ctClass.toClass()
-        } catch (e: Throwable) {
             e.printStackTrace()
         }
     }
