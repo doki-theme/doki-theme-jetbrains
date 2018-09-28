9f81c56dd57508a2775682bd481a6271af28abb1
DATBOI <alexsimons9999@gmail.com>
2018-09-28 18:20:39 -0500

    I am suprised it was that easy

--- src/main/java/io/acari/DDLC/tabs/DDLCEditorTabColorProvider.kt
+++ src/main/java/io/acari/DDLC/tabs/DDLCEditorTabColorProvider.kt
@@ -1,32 +1,33 @@
 package io.acari.DDLC.tabs
 
+import com.chrisrm.idea.actions.DarkMode
 import com.intellij.openapi.fileEditor.impl.EditorTabColorProvider
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.util.io.FileUtil
 import com.intellij.openapi.vfs.NonPhysicalFileSystem
 import com.intellij.openapi.vfs.VirtualFile
 import com.intellij.psi.search.ProjectScope
+import com.intellij.ui.ColorUtil
 import com.intellij.ui.FileColorManager
 import java.awt.Color
 
 class DDLCEditorTabColorProvider : EditorTabColorProvider {
     override fun getEditorTabColor(project: Project, file: VirtualFile): Color? {
         val colorManager = FileColorManager.getInstance(project)
-        return if (colorManager.isEnabledForTabs){
+        return if (colorManager.isEnabledForTabs) {
             val fileColor = colorManager.getFileColor(file)
-            if(isNonProject(project, file)) {
+            if (isNonProject(project, file) && fileColor != null) {
                 adjustColor(fileColor)
-            }else {
+            } else {
                 fileColor
             }
-        }
-        else null
+        } else null
     }
 
-    private fun adjustColor(fileColor: Color?): Color? {
-        println("poopy")
-        return fileColor
-    }
+    private fun adjustColor(fileColor: Color) =
+            if (DarkMode.isOn()) ColorUtil.brighter(fileColor, 10)
+            else fileColor
+
 
     private fun isNonProject(project: Project, virtualFile: VirtualFile) =
             if (virtualFile.getFileSystem() is NonPhysicalFileSystem) {
