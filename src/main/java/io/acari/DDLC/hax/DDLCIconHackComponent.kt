fd079af4f75714ac2943043ec5e64724605c8f74
DATBOI <alexsimons9999@gmail.com>
2018-11-20 18:45:48 -0600

    Thsi may work the way I want it to

--- src/main/java/io/acari/DDLC/hax/DDLCIconHackComponent.kt
+++ src/main/java/io/acari/DDLC/hax/DDLCIconHackComponent.kt
@@ -4,7 +4,6 @@ import com.chrisrm.ideaddlc.MTThemeManager
 import com.intellij.openapi.util.IconLoader
 import com.intellij.openapi.util.IconPathPatcher
 import io.acari.DDLC.DDLCApplicationInitializationComponent
-import java.util.*
 
 object DDLCIconHackComponent {
 
@@ -12,27 +11,55 @@ object DDLCIconHackComponent {
     DDLCApplicationInitializationComponent.addInitializationListener(Runnable {
       enableMaterialCoexistence()
     })
+
     MTThemeManager.addMaterialThemeActivatedListener {
       activatePatchers(it)
     }
   }
 
-  private fun activatePatchers(materialThemeActive: Boolean?) {
-
+  private fun activatePatchers(materialThemeActive: Boolean) {
+    harvestIconPatchers()
+    when (materialThemeActive && mtIconReplacers.isNotEmpty()) {
+      true -> {
+        iconReplacements.addAll(mtIconReplacers)
+      }
+      false -> {
+        iconReplacements.addAll(ddlcIconReplacers)
+      }
+    }
+    iconReplacements.addAll(iconBucket)
   }
 
-  private val mtIconReplacers = LinkedList<IconPathPatcher>()
-  private lateinit var iconReplacements: List<IconPathPatcher>
+  private val mtIconReplacers = HashSet<IconPathPatcher>()
+  private val ddlcIconReplacers = HashSet<IconPathPatcher>()
+  private val iconBucket = HashSet<IconPathPatcher>()
+  private lateinit var iconReplacements: MutableList<IconPathPatcher>
 
   private fun enableMaterialCoexistence() {
     iconReplacements = extractPatchersReference()
-    println(iconReplacements)
+    activatePatchers(true) //assuming that material ui loads first
   }
 
-  private fun extractPatchersReference(): List<IconPathPatcher> {
+  private fun harvestIconPatchers() {
+    iconReplacements.removeAll {
+      val name = it.javaClass.name
+      val containsMaterial = name.contains("com.chrisrm")
+      val containsDDLC = name.contains("ddlc")
+      if (containsMaterial && !containsDDLC) {
+        mtIconReplacers.add(it)
+      } else if (containsMaterial && containsDDLC) {
+        ddlcIconReplacers.add(it)
+      } else {
+        iconBucket.add(it)
+      }
+      true
+    }
+  }
+
+  private fun extractPatchersReference(): MutableList<IconPathPatcher> {
     val ourPatchersField = IconLoader::class.java.getDeclaredField("ourPatchers")
     ourPatchersField.isAccessible = true
-    return ourPatchersField.get(null) as List<IconPathPatcher>
+    return ourPatchersField.get(null) as MutableList<IconPathPatcher>
 
   }
 
