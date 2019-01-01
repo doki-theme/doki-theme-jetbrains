e468945317cc70dff306ec1a60530927dec7092b
DATBOI <alexsimons9999@gmail.com>
2019-01-01 11:10:39 -0600

    Bump version up to v2.2.1

--- src/main/java/com/chrisrm/ideaddlc/utils/MTUiUtils.java
+++ src/main/java/com/chrisrm/ideaddlc/utils/MTUiUtils.java
@@ -200,7 +200,7 @@ public enum MTUiUtils {
   public static String getVersion() {
      return getPlugin()
              .map(IdeaPluginDescriptor::getVersion)
-             .orElse("2.2.0");
+             .orElse("2.2.1");
   }
 
   private static Optional<String> getPluginId() {
