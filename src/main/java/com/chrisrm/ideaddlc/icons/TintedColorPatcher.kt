6e1b5844c940c7711d7e7b27b896542e2d9855c2
DATBOI <alexsimons9999@gmail.com>
2019-02-08 18:24:56 -0600

    More Kotlin, mo'h betta

---
+++ src/main/java/com/chrisrm/ideaddlc/icons/TintedColorPatcher.kt
@@ -0,0 +1,89 @@
+package com.chrisrm.ideaddlc.icons
+
+import com.chrisrm.ideaddlc.MTConfig
+import com.chrisrm.ideaddlc.listeners.AccentsListener
+import com.chrisrm.ideaddlc.listeners.MTTopics
+import com.chrisrm.ideaddlc.listeners.ThemeListener
+import com.chrisrm.ideaddlc.utils.MTAccents
+import com.intellij.ui.ColorUtil
+import com.intellij.util.SVGLoader
+import com.intellij.util.messages.MessageBusConnection
+import io.acari.DDLC.DDLCThemeFacade
+import org.jetbrains.annotations.NonNls
+import org.w3c.dom.Element
+import org.w3c.dom.Node
+import org.w3c.dom.NodeList
+
+import java.awt.*
+
+class TintedColorPatcher internal constructor(connect: MessageBusConnection) : SVGLoader.SvgColorPatcher {
+
+  init {
+    SVGLoader.setColorPatcher(this)
+    val self = this
+
+    // Listen for changes on the settings
+    connect.subscribe(MTTopics.ACCENTS, object : AccentsListener {
+      override fun accentChanged(accentColor: Color) {
+        SVGLoader.setColorPatcher(null)
+        SVGLoader.setColorPatcher(self)
+        TintedColorPatcher.refreshAccentColor(accentColor)
+      }
+    })
+
+    connect.subscribe(MTTopics.THEMES, object : ThemeListener {
+      override fun themeChanged(theme: DDLCThemeFacade) {
+        SVGLoader.setColorPatcher(null)
+        SVGLoader.setColorPatcher(self)
+        TintedColorPatcher.refreshThemeColor(theme)
+      }
+    })
+    refreshColors()
+  }
+
+  override fun patchColors(@NonNls svg: Element) {
+    @NonNls val tint = svg.getAttribute("tint")
+    @NonNls val themed = svg.getAttribute("themed")
+
+    if ("true" == tint || "fill" == tint) {
+      svg.setAttribute("fill", "#$accentColor")
+    } else if ("stroke" == tint) {
+      svg.setAttribute("stroke", "#$accentColor")
+    } else if ("true" == themed || "fill" == themed) {
+      svg.setAttribute("fill", "#$themedColor")
+    } else if ("stroke" == themed) {
+      svg.setAttribute("stroke", "#$themedColor")
+    }
+
+    val nodes = svg.childNodes
+    val length = nodes.length
+    for (i in 0 until length) {
+      val item = nodes.item(i)
+      if (item is Element) {
+        patchColors(item)
+      }
+    }
+  }
+
+  companion object {
+    @NonNls
+    private var accentColor = MTAccents.TURQUOISE.hexColor
+    @NonNls
+    private var themedColor = MTAccents.OCEANIC.hexColor
+
+    private val CONFIG = MTConfig.getInstance()
+
+    internal fun refreshAccentColor(accentColor: Color) {
+      TintedColorPatcher.accentColor = ColorUtil.toHex(accentColor)
+    }
+
+    internal fun refreshThemeColor(theme: DDLCThemeFacade) {
+      themedColor = ColorUtil.toHex(theme.theme.tintedIconColor)
+    }
+
+    private fun refreshColors() {
+      accentColor = CONFIG.accentColor
+      themedColor = ColorUtil.toHex(CONFIG.selectedTheme.theme.tintedIconColor)
+    }
+  }
+}
