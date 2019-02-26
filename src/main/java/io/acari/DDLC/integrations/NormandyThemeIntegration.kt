d4299c955bf10f239ac7d1f98a9687dbbae05cba
DATBOI <alexsimons9999@gmail.com>
2019-02-25 18:08:10 -0600

    More dancing around the issue until I get it in all the IDEs

--- src/main/java/io/acari/DDLC/integrations/NormandyThemeIntegration.kt
+++ src/main/java/io/acari/DDLC/integrations/NormandyThemeIntegration.kt
@@ -6,25 +6,34 @@ import com.intellij.compiler.server.CustomBuilderMessageHandler
 import com.intellij.ui.ColorUtil
 import com.intellij.util.messages.MessageBus
 import io.acari.DDLC.DDLCThemeFacade
+import io.acari.DDLC.LegacySupportUtility
 import io.acari.DDLC.themes.AccentChangedInformation
 import io.acari.DDLC.themes.ThemeChangedInformation
 import java.awt.Color
 
+const val OVERRIDE_CLASS = "com.intellij.compiler.server.CustomBuilderMessageHandler"
+
 object NormandyThemeIntegration {
   private val gson = Gson()
 
+  val isEnabled = LegacySupportUtility.orGetLegacy(OVERRIDE_CLASS, {true}, {false})
+
   fun themeChanged(messageBus: MessageBus, newTheme: DDLCThemeFacade) {
-    messageBus.syncPublisher(CustomBuilderMessageHandler.TOPIC)
-        .messageReceived("io.acari.DDLCTheme",
-            "Theme Changed",
-            gson.toJson(createThemeDeltas(newTheme.theme)))
+    if(isEnabled){
+      messageBus.syncPublisher(CustomBuilderMessageHandler.TOPIC)
+          .messageReceived("io.acari.DDLCTheme",
+              "Theme Changed",
+              gson.toJson(createThemeDeltas(newTheme.theme)))
+    }
   }
 
   fun accentChanged(messageBus: MessageBus, accentColor: Color) {
-    messageBus.syncPublisher(CustomBuilderMessageHandler.TOPIC)
-        .messageReceived("io.acari.DDLCTheme",
-            "Accent Changed",
-            gson.toJson(createAccentDeltas(accentColor)))
+    if(isEnabled){
+      messageBus.syncPublisher(CustomBuilderMessageHandler.TOPIC)
+          .messageReceived("io.acari.DDLCTheme",
+              "Accent Changed",
+              gson.toJson(createAccentDeltas(accentColor)))
+    }
   }
 
   private fun createAccentDeltas(accentColor: Color): AccentChangedInformation =
