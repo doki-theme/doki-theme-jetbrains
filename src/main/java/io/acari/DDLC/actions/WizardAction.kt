99009905d23a00d16aad7ad55af2233f0911c675
DATBOI <alexsimons9999@gmail.com>
2019-03-07 18:21:34 -0600

    One wizard action

---
+++ src/main/java/io/acari/DDLC/actions/WizardAction.kt
@@ -0,0 +1,14 @@
+package io.acari.DDLC.actions
+
+import com.intellij.openapi.actionSystem.AnAction
+import com.intellij.openapi.actionSystem.AnActionEvent
+import io.acari.DDLC.wizard.DDLCWizardDialog
+import io.acari.DDLC.wizard.DDLCWizardStepsProvider
+
+class WizardAction : AnAction() {
+  override fun actionPerformed(e: AnActionEvent) {
+    DDLCWizardDialog(DDLCWizardStepsProvider()).show()
+  }
+
+
+}
