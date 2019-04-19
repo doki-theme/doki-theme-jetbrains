14a95f5e839668556445bd28e4992a13021291ab
DATBOI <alexsimons9999@gmail.com>
2019-04-19 04:43:27 -0500

    Added notification to the event log as well

--- src/main/java/com/chrisrm/ideaddlc/notifications/Notify.java
+++ src/main/java/com/chrisrm/ideaddlc/notifications/Notify.java
@@ -154,6 +154,7 @@ public enum Notify {
       } catch (final NoSuchMethodError | NoClassDefFoundError | NoSuchFieldError e) {
         notification.notify(project);
       }
+      Notifications.Bus.notify(notification);
     }
   }
 }
