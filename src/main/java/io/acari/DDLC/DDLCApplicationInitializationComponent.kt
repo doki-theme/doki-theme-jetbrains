5e370547a14750191ad09dd7dd3c380e778c09fd
DATBOI <alexsimons9999@gmail.com>
2019-04-20 18:06:01 -0500

    Putting back the deprecated API for application initalization

--- src/main/java/io/acari/DDLC/DDLCApplicationInitializationComponent.kt
+++ src/main/java/io/acari/DDLC/DDLCApplicationInitializationComponent.kt
@@ -1,43 +1,27 @@
 package io.acari.DDLC
 
 import com.chrisrm.ideaddlc.MTThemeManager
-import com.intellij.openapi.Disposable
-import com.intellij.openapi.application.ApplicationActivationListener
-import com.intellij.openapi.application.ApplicationManager
-import com.intellij.openapi.wm.IdeFrame
-import com.intellij.util.messages.MessageBusConnection
+import com.intellij.openapi.components.ApplicationComponent
 import io.acari.DDLC.chibi.ChibiOrchestrator
 import io.acari.DDLC.wizard.DDLCWizardDialog
 import io.acari.DDLC.wizard.DDLCWizardStepsProvider
 import java.util.*
 
-class DDLCApplicationInitializationComponent : Disposable {
-
-  private val messageBusConnection: MessageBusConnection = ApplicationManager.getApplication().messageBus.connect()
-
-  init {
-    messageBusConnection.subscribe(ApplicationActivationListener.TOPIC, object: ApplicationActivationListener {
-      override fun applicationActivated(ideFrame: IdeFrame) {
-        initComponent()
-      }
-    })
-  }
-
+class DDLCApplicationInitializationComponent : ApplicationComponent {
   companion object {
     private val listeners = LinkedList<Runnable>()
+
     fun addInitializationListener(callback: Runnable){
       listeners.add(callback)
     }
-
     fun notifyListeners(){
       listeners.forEach { it.run() }
     }
   }
-  override fun dispose() {
-    messageBusConnection.dispose()
-  }
 
-  fun initComponent() {
+  override fun initComponent() {
+    super.initComponent()
+
     //This is stupid, but the manager does not get created until after.
     addInitializationListener(MTThemeManager.getInstance().createListener())
     ChibiOrchestrator.init()
