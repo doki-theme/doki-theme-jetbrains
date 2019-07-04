0c51295618be3d0940cc6b3fe30b78ecf2f3bee4
DATBOI <alexsimons9999@gmail.com>
2019-07-04 18:12:18 -0500

    Finna put the wyla frame in

--- src/main/java/io/acari/DDLC/AnthroThemes.kt
+++ src/main/java/io/acari/DDLC/AnthroThemes.kt
@@ -3,25 +3,28 @@ package io.acari.DDLC
 import io.acari.DDLC.themes.AnthroTheme
 import io.acari.DDLC.themes.anthro.NeeraTheme
 import io.acari.DDLC.themes.anthro.SanyaTheme
+import io.acari.DDLC.themes.anthro.WylaTheme
 import java.util.*
 
 //NEERA,
 //SANYA,
+//WYLA,
 //SYRENA,
+//CLEO,
 //FEN,
 //ELENIEL,
-//CLEO,
-//WYLA,
 //ZAHRA,
 //JESS,
 
 object AnthroThemes {
   val NEERA: AnthroTheme = NeeraTheme()
   val SANYA: AnthroTheme = SanyaTheme()
+  val WYLA: AnthroTheme = WylaTheme()
 
   fun getAllThemes(): List<DDLCThemeFacade> = listOf(
       NEERA,
-      SANYA
+      SANYA,
+      WYLA
   )
 
 }
---
+++ src/main/java/io/acari/DDLC/themes/anthro/WylaTheme.kt
@@ -0,0 +1,65 @@
+package io.acari.DDLC.themes.anthro
+
+import com.chrisrm.ideaddlc.utils.MTAccents
+import io.acari.DDLC.themes.AnthroTheme
+import javax.swing.plaf.ColorUIResource
+
+class WylaTheme : AnthroTheme("wyla", "Wyla", true, "Wyla") {
+
+  override fun getChibi(): String = "athro/neera_temp.png"
+
+  override fun getNormalChibi(): String = "athro/neera_temp.png"
+
+  override fun getBackgroundColorString(): String = "362130"
+
+  //todo: remove these
+  override fun getClubMember(): String = "sayori.png"
+
+  override fun joyfulClubMember(): String = "sayori_joy.png"
+
+  override fun getSecondaryBackgroundColorString(): String = "362839"
+
+  override fun getSecondaryForegroundColorString(): String = "635959"
+
+  override fun getSelectionForegroundColorString(): String = "E2CB14"
+
+  override fun getSelectionBackgroundColorString(): String = "F0E891"
+
+  override fun getTreeSelectionBackgroundColorString(): String = "ECD95A"
+
+  override fun getInactiveColorString(): String = "FFFFBC"
+
+  override fun getMenuItemForegroundColor(): String = "635959"
+
+  override fun getMenuBarSelectionForegroundColorString(): String = "ffffff"
+
+  override fun getMenuBarSelectionBackgroundColorString(): String = "C5BD68"
+
+  override fun getNotificationsColorString(): String = "FFFBC7"
+
+  override fun getHighlightColorString(): String = "FFEE96"
+
+  override fun getContrastColorString(): String = "342629"
+
+  override fun getBorderColorString(): String = "6c113a"
+
+  override fun getEditorTabColorString(): String = contrastColorString
+
+  override fun getButtonBackgroundColor(): String = contrastColorString
+
+  override fun getButtonForegroundColor(): String = "635959"
+
+  override fun getAccentColor(): String = MTAccents.ACID_LIME.hexColor
+
+  override fun getBackgroundColorResource(): ColorUIResource = ColorUIResource(0x312428)
+
+  override fun getForegroundColorResource(): ColorUIResource = ColorUIResource(0x635959)
+
+  override fun getTableSelectedColorString(): String = "FFFBA3"
+
+  override fun getStartColor(): String = "6d4466"
+
+  override fun getStopColor(): String = "ec1f62"
+
+  override fun getNonProjectFileScopeColor(): String = "56412d"
+}
