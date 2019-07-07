e17a97bc6cc36fb5351fc1dbb66ac1c775647746
DATBOI <alexsimons9999@gmail.com>
2019-07-07 16:50:53 -0500

    Small touch up on sanya

--- src/main/java/io/acari/DDLC/themes/anthro/SanyaTheme.kt
+++ src/main/java/io/acari/DDLC/themes/anthro/SanyaTheme.kt
@@ -41,7 +41,7 @@ class SanyaTheme : AnthroTheme("sanya", "Sanya", true, "Sanya") {
 
   override fun getContrastColorString(): String = "341F2D"
 
-  override fun getBorderColorString(): String = "6c113a"
+  override fun getBorderColorString(): String = "621c3c"
 
   override fun getEditorTabColorString(): String = contrastColorString
 
