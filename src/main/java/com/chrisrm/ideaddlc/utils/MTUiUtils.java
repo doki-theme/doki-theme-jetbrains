9ecaa271dc6d722c290529844898c2806ab88cc8
DATBOI <alexsimons9999@gmail.com>
2019-07-30 05:22:34 -0500

    Bumped version to v4.0.0-beta

--- src/main/java/com/chrisrm/ideaddlc/utils/MTUiUtils.java
+++ src/main/java/com/chrisrm/ideaddlc/utils/MTUiUtils.java
@@ -200,7 +200,7 @@ public enum MTUiUtils {
   public static String getVersion() {
      return getPlugin()
              .map(IdeaPluginDescriptor::getVersion)
-             .orElse("3.5.0");
+             .orElse("4.0.0-beta");
   }
 
   private static Optional<String> getPluginId() {
