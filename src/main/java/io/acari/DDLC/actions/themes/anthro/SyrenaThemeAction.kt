d282bf8c1d56e90de020a11f353b3ea7a735d3c7
DATBOI <alexsimons9999@gmail.com>
2019-08-01 06:20:44 -0500

    How did that get there?

--- src/main/java/io/acari/DDLC/actions/themes/anthro/SyrenaThemeAction.kt
+++ src/main/java/io/acari/DDLC/actions/themes/anthro/SyrenaThemeAction.kt
@@ -1,7 +1,6 @@
 package io.acari.DDLC.actions.themes.anthro
 
 import com.chrisrm.ideaddlc.actions.accents.DokiMagmaAccentAction
-import com.chrisrm.ideaddlc.actions.accents.MTTomatoAccentAction
 import io.acari.DDLC.AnthroThemes
 import io.acari.DDLC.actions.themes.literature.club.ClubMemberThemeAction
 
