f1215aa9f56b3a6f763559ea5efbf494316c451a
DATBOI <alexsimons9999@gmail.com>
2019-08-01 05:39:54 -0500

    Bumped version to v3.5.0 and updated notification message

--- src/main/java/io/acari/DDLC/Constants.kt
+++ src/main/java/io/acari/DDLC/Constants.kt
@@ -8,7 +8,10 @@ const val DARK_MODE_CONSTANT: String = "Ḑ̶͍̲̳̫͗̚͠ä̵͉̠̬͙̤͚͆͆
 val UPDATE_MESSAGE: String = """
       What's New?<br>
       <ul>
-      <li>Better MacOS 2019.2 Build Support!</li>
+      <li>Updated to Material UI 4.2.1!
+        <ul>
+          <li>Also supporting better integrations (less theme color bleed)</li>
+        </li>
       <br>
       Thanks again for downloading <b>The Doki-Doki Theme</b>! •‿•<br>
       <br>Please see the <a href="https://github.com/cyclic-reference/ddlc-jetbrains-theme/blob/master/docs/CHANGELOG.md">Changelog</a> for more details.
