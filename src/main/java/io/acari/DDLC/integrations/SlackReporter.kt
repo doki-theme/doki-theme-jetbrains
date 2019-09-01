bc6ed3c0fbcf1debaa796320c827d2080fcecf84
DATBOI <alexsimons9999@gmail.com>
2019-09-01 17:49:22 -0500

    Moar info

--- src/main/java/io/acari/DDLC/integrations/SlackReporter.kt
+++ src/main/java/io/acari/DDLC/integrations/SlackReporter.kt
@@ -1,50 +1,55 @@
 package io.acari.DDLC.integrations
 
+import com.chrisrm.ideaddlc.utils.MTUiUtils
 import com.google.gson.GsonBuilder
 import com.intellij.ide.IdeBundle
+import com.intellij.ide.plugins.PluginManagerCore
 import com.intellij.openapi.application.ApplicationNamesInfo
 import com.intellij.openapi.application.ex.ApplicationInfoEx
 import com.intellij.openapi.application.impl.ApplicationInfoImpl
 import com.intellij.openapi.diagnostic.ErrorReportSubmitter
 import com.intellij.openapi.diagnostic.IdeaLoggingEvent
 import com.intellij.openapi.diagnostic.SubmittedReportInfo
-import com.intellij.ui.LicensingFacade
+import com.intellij.openapi.util.SystemInfo
+import com.intellij.openapi.util.io.FileUtilRt
+import com.intellij.openapi.util.registry.Registry
 import com.intellij.util.Consumer
 import com.intellij.util.text.DateFormatUtil
-import org.apache.commons.httpclient.methods.PostMethod
-import org.apache.http.HttpEntity
+import io.acari.DDLC.DDLCConfig
 import org.apache.http.client.methods.HttpPost
-import org.apache.http.client.methods.HttpUriRequest
 import org.apache.http.entity.ContentType
 import org.apache.http.entity.StringEntity
 import org.apache.http.impl.client.HttpClients
 import java.awt.Component
+import java.lang.management.ManagementFactory
 import java.text.SimpleDateFormat
-import java.util.ArrayList
+import java.util.*
+import java.util.stream.Collectors
 
-class SlackReporter: ErrorReportSubmitter() {
-  companion object{
+class SlackReporter : ErrorReportSubmitter() {
+  companion object {
     val httpClient = HttpClients.createMinimal()
     val slackWebhook = "https://hooks.slack.com/services/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"
     val gson = GsonBuilder().create()
   }
+
   override fun getReportActionText(): String = "Report Anonymously"
 
   override fun submit(events: Array<out IdeaLoggingEvent>, additionalInfo: String?, parentComponent: Component, consumer: Consumer<SubmittedReportInfo>): Boolean {
     return try {
       val httpPost = HttpPost(slackWebhook)
       val eventMessages = events.map {
-        """
-          Data: ${it.data}
-          Message: ${it.message}
-          Throwable Text: ${it.throwableText}
-        """.trimIndent()
+        """_Message_: ${it.message}
+          |_Throwable Text_: ${it.throwableText}""".trimMargin()
       }.joinToString(separator = "\n")
-      val supplementedInfo = if(additionalInfo != null) """
-        $eventMessages
+      val supplementedInfo = if (additionalInfo != null) """$eventMessages
         Additional Info: $additionalInfo
       """.trimIndent() else eventMessages
-      val formattedMessage = "```$supplementedInfo```"
+      val formattedMessage = """
+        *System Info*:
+        ```${getSystemInfo()}```
+        *Error Info*:
+        $supplementedInfo""".trimIndent()
       val message = SlackMessage(formattedMessage)
       httpPost.entity = StringEntity(gson.toJson(message), ContentType.APPLICATION_JSON)
       httpClient.execute(httpPost)
@@ -55,57 +60,55 @@ class SlackReporter: ErrorReportSubmitter() {
   }
 
 
+  private fun getSystemInfo(): String {
+    val myInfo = StringBuilder()
+    val appInfo = ApplicationInfoEx.getInstanceEx() as ApplicationInfoImpl
+    var appName = appInfo.fullApplicationName
+    val edition = ApplicationNamesInfo.getInstance().editionName
+    if (edition != null) appName += " ($edition)"
+    myInfo.append(appName).append("\n")
 
-  fun getSystemInfo(){
-//    val myInfo = StringBuilder()
-//    val appInfo = ApplicationInfoEx.getInstanceEx() as ApplicationInfoImpl
-//    var appName = appInfo.getFullApplicationName()
-//    val edition = ApplicationNamesInfo.getInstance().editionName
-//    if (edition != null) appName += " ($edition)"
-//    myInfo.append(myLines.get(myLines.size() - 1).getText()).append("\n");
-//
-//    var buildInfo = IdeBundle.message("about.box.build.number", appInfo.getBuild().asString())
-//    val cal = appInfo.getBuildDate()
-//    var buildDate = ""
-//    if (appInfo.getBuild().isSnapshot()) {
-//      buildDate = SimpleDateFormat("HH:mm, ").format(cal.getTime())
-//    }
-//    buildDate += DateFormatUtil.formatAboutDialogDate(cal.getTime())
-//    buildInfo += IdeBundle.message("about.box.build.date", buildDate)
-//    myLines.add(AboutBoxLine(buildInfo))
-//    myInfo.append(myLines.get(myLines.size() - 1).getText()).append("\n");
-//
-//
-//
-//    myLines.add(AboutBoxLine(""))
-//
-//    val properties = System.getProperties()
-//    val javaVersion = properties.getProperty("java.runtime.version", properties.getProperty("java.version", "unknown"))
-//    val arch = properties.getProperty("os.arch", "")
-//    myLines.add(AboutBoxLine(IdeBundle.message("about.box.jre", javaVersion, arch)))
-//    myInfo.append(myLines.get(myLines.size() - 1).getText()).append("\n");
-//
-//    val vmVersion = properties.getProperty("java.vm.name", "unknown")
-//    val vmVendor = properties.getProperty("java.vendor", "unknown")
-//    myLines.add(AboutBoxLine(IdeBundle.message("about.box.vm", vmVersion, vmVendor)))
-//    myInfo.append(myLines.get(myLines.size() - 1).getText()).append("\n");
-//
-//    myLines.add(AboutBoxLine(""))
-//    myLines.add(AboutBoxLine(""))
-//    myLines.add(AboutBoxLine(IdeBundle.message("about.box.powered.by") + " ").keepWithNext())
-//
-//    val thirdPartyLibraries = loadThirdPartyLibraries()
-//    if (thirdPartyLibraries != null) {
-//      myLines.add(AboutBoxLine(IdeBundle.message("about.box.open.source.software")
-//      ) { showOpenSoftwareSources(thirdPartyLibraries!!) })
-//    } else {
-//      // When compiled from sources, third-party-libraries.html file isn't generated, so window can't be shown
-//      myLines.add(AboutBoxLine(IdeBundle.message("about.box.open.source.software")))
-//    }
+    var buildInfo = IdeBundle.message("about.box.build.number", appInfo.build.asString())
+    val cal = appInfo.buildDate
+    var buildDate = ""
+    if (appInfo.build.isSnapshot) {
+      buildDate = SimpleDateFormat("HH:mm, ").format(cal.time)
+    }
+    buildDate += DateFormatUtil.formatAboutDialogDate(cal.time)
+    buildInfo += IdeBundle.message("about.box.build.date", buildDate)
+    myInfo.append(buildInfo).append("\n")
 
+    val properties = System.getProperties()
+    val javaVersion = properties.getProperty("java.runtime.version", properties.getProperty("java.version", "unknown"))
+    val arch = properties.getProperty("os.arch", "")
+    myInfo.append(IdeBundle.message("about.box.jre", javaVersion, arch)).append("\n")
+
+    val vmVersion = properties.getProperty("java.vm.name", "unknown")
+    val vmVendor = properties.getProperty("java.vendor", "unknown")
+    myInfo.append(IdeBundle.message("about.box.vm", vmVersion, vmVendor)).append("\n")
+
+    val dokiConfig = DDLCConfig.getInstance()
+    return """$myInfo${extraInfo()}
+      |Doki-Doki Config: ${dokiConfig.asJson()}
+      |Doki-Doki Version: ${MTUiUtils.getVersion()}
+      |Doki-Doki DarkMode: ${dokiConfig.isDarkMode}""".trimMargin()
   }
 
+  private fun extraInfo(): String {
+    return SystemInfo.getOsNameAndVersion() + "\n" +
+        "GC: " + ManagementFactory.getGarbageCollectorMXBeans().stream()
+        .map<String> { it.name }.collect(Collectors.joining(",")) + "\n" +
 
+        "Memory: " + Runtime.getRuntime().maxMemory() / FileUtilRt.MEGABYTE + "M" + "\n" +
+
+        "Cores: " + Runtime.getRuntime().availableProcessors() + "\n" +
+
+        "Registry: " + Registry.getAll().stream().filter { it.isChangedFromDefault }
+        .map { v -> v.key + "=" + v.asString() }.collect(Collectors.joining(",")) + "\n" +
+
+        "Non-Bundled Plugins: " + Arrays.stream(PluginManagerCore.getPlugins()).filter { p -> !p.isBundled && p.isEnabled }
+        .map { p -> p.pluginId.idString }.collect(Collectors.joining(","))
+  }
 
 }
 
