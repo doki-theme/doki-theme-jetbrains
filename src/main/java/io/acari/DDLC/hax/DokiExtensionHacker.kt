628765bba684f0a2209c20726cbf48cc8828dc1f
DATBOI <alexsimons9999@gmail.com>
2019-10-30 05:34:55 -0500

    Yay! It work

--- src/main/java/io/acari/DDLC/hax/DokiExtensionHacker.kt
+++ src/main/java/io/acari/DDLC/hax/DokiExtensionHacker.kt
@@ -24,95 +24,95 @@ object DokiExtensionHacker {
   private var nonMaterialAdapters: MutableList<Any?> = ArrayList()
   private var dokiAdapters: MutableList<Any?> = ArrayList()
 
-  init{
-    MTThemeManager.addMaterialThemeActivatedListener {
-      handleMaterialThemeActivated(it)
-    }
-  }
-
-  private fun handleMaterialThemeActivated(materialThemeActive: Boolean) {
-      if(materialThemeActive){
-        putBackMaterialExtensions()
-      } else {
-        removeMaterialExtensions()
-      }
-  }
-
-  private fun removeMaterialExtensions() {
-    if(!dokiExtensions.isEmpty()){
-      val point = IconProvider.EXTENSION_POINT_NAME.getPoint(null)
-      val listToAddTo = ArrayList(dokiExtensions)
-      listToAddTo.addAll(nonMaterialExtensions)
-      cacheFeildToHack.set(point, ContainerUtil.immutableList(listToAddTo))
-    }
-  }
-
-  private fun putBackMaterialExtensions() {
-    if(!materialExtensions.isEmpty()){
-      val point = IconProvider.EXTENSION_POINT_NAME.getPoint(null)
-      val listToAddTo = ArrayList(materialExtensions)
-      listToAddTo.addAll(nonMaterialExtensions)
-      cacheFeildToHack.set(point, ContainerUtil.immutableList(listToAddTo))
-    }
-
-  }
+//  init{
+//    MTThemeManager.addMaterialThemeActivatedListener {
+//      handleMaterialThemeActivated(it)
+//    }
+//  }
+//
+//  private fun handleMaterialThemeActivated(materialThemeActive: Boolean) {
+//      if(materialThemeActive){
+//        putBackMaterialExtensions()
+//      } else {
+//        removeMaterialExtensions()
+//      }
+//  }
+//
+//  private fun removeMaterialExtensions() {
+//    if(!dokiExtensions.isEmpty()){
+//      val point = IconProvider.EXTENSION_POINT_NAME.getPoint(null)
+//      val listToAddTo = ArrayList(dokiExtensions)
+//      listToAddTo.addAll(nonMaterialExtensions)
+//      cacheFeildToHack.set(point, ContainerUtil.immutableList(listToAddTo))
+//    }
+//  }
+//
+//  private fun putBackMaterialExtensions() {
+//    if(!materialExtensions.isEmpty()){
+//      val point = IconProvider.EXTENSION_POINT_NAME.getPoint(null)
+//      val listToAddTo = ArrayList(materialExtensions)
+//      listToAddTo.addAll(nonMaterialExtensions)
+//      cacheFeildToHack.set(point, ContainerUtil.immutableList(listToAddTo))
+//    }
+//
+//  }
 
   fun hackExtensions() {
-    LegacySupportUtility.invokeClassSafely("com.intellij.ide.ui.laf.darcula.ui.DarculaSeparatorUI", Runner {
-      mutilateExtensionPoints()
-    })
+//    LegacySupportUtility.invokeClassSafely("com.intellij.ide.ui.laf.darcula.ui.DarculaSeparatorUI", Runner {
+//      mutilateExtensionPoints()
+//    })
   }
 
-  private fun mutilateExtensionPoints() {
-    val classToHack = ExtensionPointImpl::class.java
-    cacheFeildToHack = classToHack.declaredFields.stream().filter { it.name == "myExtensionsCache" }.findFirst().get()
-
-    val (dokiExtractedExtensions, materialExtractedExtensions, nonMaterialExtractedExtensions) =
-        mutilateField(cacheFeildToHack) { it?.javaClass?.name ?: ""}
-    dokiExtensions = dokiExtractedExtensions
-    materialExtensions = materialExtractedExtensions
-    nonMaterialExtensions = nonMaterialExtractedExtensions
-
-    adapterFeildToHack = classToHack.declaredFields.stream().filter { it.name == "myAdapters" }.findFirst().get()
-    val (dokiExtractedAdapters, materialExtractedAdapters, nonMaterialExtractedAdapters) =
-        mutilateField(adapterFeildToHack) { (it as? ExtensionComponentAdapter)?.assignableToClassName ?: ""}
-    dokiAdapters = dokiExtractedAdapters
-    materialAdapters = materialExtractedAdapters
-    nonMaterialAdapters = nonMaterialExtractedAdapters
-  }
-
-  private fun mutilateField(cacheFeildToHack: Field, nameExtractor: (Any?)->String): Triple<ImmutableList<Any?>, ImmutableList<Any?>, ImmutableList<Any?>> {
-    cacheFeildToHack.isAccessible = true
-    val point = IconProvider.EXTENSION_POINT_NAME.getPoint(null)
-    point.extensionList
-    val cache = cacheFeildToHack.get(point)
-    if (cache is List<*>) {
-      val partitionedIconProviders: Map<String, List<Any?>> = cache.stream().collect(
-          Collectors.groupingBy {
-            val name = nameExtractor(it)
-            val containsMaterial = name.contains("com.chrisrm")
-            val containsDDLC = name.contains("ddlc")
-            if (!containsDDLC && containsMaterial) "MATERIAL"
-            else if (containsDDLC && containsMaterial) "DOKI-DOKI"
-            else "OTHER"
-          }
-      )
-
-      val nonMaterialExtensions_2 = ContainerUtil.immutableList(partitionedIconProviders["OTHER"]
-          ?: Collections.emptyList())
-      val materialExtensions_2 = ContainerUtil.immutableList(partitionedIconProviders["MATERIAL"]
-          ?: Collections.emptyList())
-      val dokiExtensions_2 = ContainerUtil.immutableList(partitionedIconProviders["DOKI-DOKI"]
-          ?: Collections.emptyList())
-
-      val arrayList = ArrayList(dokiExtensions_2)
-      arrayList.addAll(nonMaterialExtensions_2)
-      cacheFeildToHack.set(point, ContainerUtil.immutableList(arrayList))
-      return Triple(dokiExtensions_2,
-          materialExtensions_2,
-          nonMaterialExtensions_2)
-    }
-    val emptyList = ContainerUtil.immutableList(mutableListOf<Any?>())
-    return Triple(emptyList,emptyList,emptyList)
-  }
+//  private fun mutilateExtensionPoints() {
+//    val classToHack = ExtensionPointImpl::class.java
+//    cacheFeildToHack = classToHack.declaredFields.stream().filter { it.name == "myExtensionsCache" }.findFirst().get()
+//
+//    val (dokiExtractedExtensions, materialExtractedExtensions, nonMaterialExtractedExtensions) =
+//        mutilateField(cacheFeildToHack) { it?.javaClass?.name ?: ""}
+//    dokiExtensions = dokiExtractedExtensions
+//    materialExtensions = materialExtractedExtensions
+//    nonMaterialExtensions = nonMaterialExtractedExtensions
+//
+//    adapterFeildToHack = classToHack.declaredFields.stream().filter { it.name == "myAdapters" }.findFirst().get()
+//    val (dokiExtractedAdapters, materialExtractedAdapters, nonMaterialExtractedAdapters) =
+//        mutilateField(adapterFeildToHack) { (it as? ExtensionComponentAdapter)?.assignableToClassName ?: ""}
+//    dokiAdapters = dokiExtractedAdapters
+//    materialAdapters = materialExtractedAdapters
+//    nonMaterialAdapters = nonMaterialExtractedAdapters
+//  }
+//
+//  private fun mutilateField(cacheFeildToHack: Field, nameExtractor: (Any?)->String): Triple<ImmutableList<Any?>, ImmutableList<Any?>, ImmutableList<Any?>> {
+//    cacheFeildToHack.isAccessible = true
+//    val point = IconProvider.EXTENSION_POINT_NAME.getPoint(null)
+//    point.extensionList
+//    val cache = cacheFeildToHack.get(point)
+//    if (cache is List<*>) {
+//      val partitionedIconProviders: Map<String, List<Any?>> = cache.stream().collect(
+//          Collectors.groupingBy {
+//            val name = nameExtractor(it)
+//            val containsMaterial = name.contains("com.chrisrm")
+//            val containsDDLC = name.contains("ddlc")
+//            if (!containsDDLC && containsMaterial) "MATERIAL"
+//            else if (containsDDLC && containsMaterial) "DOKI-DOKI"
+//            else "OTHER"
+//          }
+//      )
+//
+//      val nonMaterialExtensions_2 = ContainerUtil.immutableList(partitionedIconProviders["OTHER"]
+//          ?: Collections.emptyList())
+//      val materialExtensions_2 = ContainerUtil.immutableList(partitionedIconProviders["MATERIAL"]
+//          ?: Collections.emptyList())
+//      val dokiExtensions_2 = ContainerUtil.immutableList(partitionedIconProviders["DOKI-DOKI"]
+//          ?: Collections.emptyList())
+//
+//      val arrayList = ArrayList(dokiExtensions_2)
+//      arrayList.addAll(nonMaterialExtensions_2)
+//      cacheFeildToHack.set(point, ContainerUtil.immutableList(arrayList))
+//      return Triple(dokiExtensions_2,
+//          materialExtensions_2,
+//          nonMaterialExtensions_2)
+//    }
+//    val emptyList = ContainerUtil.immutableList(mutableListOf<Any?>())
+//    return Triple(emptyList,emptyList,emptyList)
+//  }
 }
