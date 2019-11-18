9ea7bbd412e32a93533843b2ed6fbd539fc9527c
DATBOI <alexsimons9999@gmail.com>
2019-11-18 05:26:12 -0600

    More able to change chibi on changed look and feel

--- DDLCTheme/src/main/kotlin/io/acari/doki/chibi/ChibiComponent.kt
+++ DDLCTheme/src/main/kotlin/io/acari/doki/chibi/ChibiComponent.kt
@@ -1,15 +1,30 @@
 package io.acari.doki.chibi
 
+import com.intellij.ide.ui.LafManagerListener
 import com.intellij.ide.ui.laf.LafManagerImpl
+import com.intellij.openapi.Disposable
+import com.intellij.openapi.application.ApplicationManager
 import io.acari.doki.themes.DokiThemes
+import javax.swing.UIManager
 
-class ChibiComponent {
+class ChibiComponent: Disposable {
+    val connection = ApplicationManager.getApplication().messageBus.connect()
 
     init {
-        val currentLaf = LafManagerImpl.getInstance().currentLookAndFeel
+        processLaf(LafManagerImpl.getInstance().currentLookAndFeel)
+        connection.subscribe(LafManagerListener.TOPIC, LafManagerListener {
+            processLaf(it.currentLookAndFeel)
+        }) 
+    }
+
+    private fun processLaf(currentLaf: UIManager.LookAndFeelInfo?) {
         DokiThemes.processLaf(currentLaf)
             .ifPresent {
                 ChibiService.instance.activateForTheme(it)
             }
+    }
+
+    override fun dispose() {
+        connection.dispose()
     }
 }
