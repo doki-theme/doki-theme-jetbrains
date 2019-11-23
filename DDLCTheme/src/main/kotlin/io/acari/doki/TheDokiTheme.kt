9b2dbd5a9abe9b700e9ff791e19d5a7761d4bbf9
DATBOI <alexsimons9999@gmail.com>
2019-11-23 15:30:30 -0600

    More able to switch file scopes on laf change

--- DDLCTheme/src/main/kotlin/io/acari/doki/TheDokiTheme.kt
+++ DDLCTheme/src/main/kotlin/io/acari/doki/TheDokiTheme.kt
@@ -1,22 +1,31 @@
 package io.acari.doki
 
+import com.intellij.ide.ui.LafManagerListener
 import com.intellij.openapi.Disposable
 import com.intellij.openapi.application.ApplicationManager
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.impl.ProjectLifecycleListener
-import io.acari.doki.laf.DokiAddFileColorsAction
+import io.acari.doki.laf.DokiAddFileColorsAction.setFileScopes
 import io.acari.doki.themes.ThemeManager
 
-class TheDokiTheme: Disposable {
-    val connection = ApplicationManager.getApplication().messageBus.connect()
+class TheDokiTheme : Disposable {
+    private val connection = ApplicationManager.getApplication().messageBus.connect()
+    lateinit var project: Project
 
     init {
         ThemeManager.instance
-        connection.subscribe(ProjectLifecycleListener.TOPIC, object: ProjectLifecycleListener{
+        connection.subscribe(LafManagerListener.TOPIC, LafManagerListener {
+            //todo: opt in to colors
+            if (::project.isInitialized) {
+                setFileScopes(project)
+            }
+        })
+        val self = this
+        connection.subscribe(ProjectLifecycleListener.TOPIC, object : ProjectLifecycleListener {
             override fun projectComponentsInitialized(project: Project) {
-
+                self.project = project
                 //todo: opt in to colors
-                DokiAddFileColorsAction.setFileScopes(project)
+                setFileScopes(project)
             }
         })
     }
