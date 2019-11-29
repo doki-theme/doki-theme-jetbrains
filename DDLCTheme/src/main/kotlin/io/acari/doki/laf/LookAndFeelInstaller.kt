b295bc2125a70bbc99e5dab0da3e4a21098fb58c
DATBOI <alexsimons9999@gmail.com>
2019-11-29 17:23:09 -0600

    New fancy tree icons!

--- DDLCTheme/src/main/kotlin/io/acari/doki/laf/LookAndFeelInstaller.kt
+++ DDLCTheme/src/main/kotlin/io/acari/doki/laf/LookAndFeelInstaller.kt
@@ -1,5 +1,6 @@
 package io.acari.doki.laf
 
+import io.acari.doki.icon.DokiIcons
 import io.acari.doki.ui.TitlePaneUI
 import io.acari.doki.ui.ToggleButtonUI
 import javax.swing.UIManager
@@ -8,8 +9,17 @@ import javax.swing.UIManager
 object LookAndFeelInstaller {
 
   fun installAllUIComponents() {
+    installIcons()
     installTitlePane()
     installButtons()
+  }
+
+  private fun installIcons() {
+    val defaults = UIManager.getLookAndFeelDefaults()
+    defaults[DokiIcons.Tree.COLLAPSED_KEY] = DokiIcons.Tree.COLLAPSED
+    defaults[DokiIcons.Tree.SELECTED_COLLAPSED_KEY] = DokiIcons.Tree.COLLAPSED
+    defaults[DokiIcons.Tree.EXPANDED_KEY] = DokiIcons.Tree.EXPANDED
+    defaults[DokiIcons.Tree.SELECTED_EXPANDED_KEY] = DokiIcons.Tree.EXPANDED
   }
 
   private fun installButtons() {
