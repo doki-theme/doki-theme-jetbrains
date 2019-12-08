6a7875e084dfc20256413b4823e57f18f59757e9
DATBOI <alexsimons9999@gmail.com>
2019-12-08 10:53:45 -0600

    Hmm, I think I may have to go to the drawing board again

--- DDLCTheme/src/main/kotlin/io/acari/doki/themes/DokiThemes.kt
+++ DDLCTheme/src/main/kotlin/io/acari/doki/themes/DokiThemes.kt
@@ -1,30 +1,45 @@
 package io.acari.doki.themes
 
+import com.google.gson.Gson
+import com.intellij.ide.ui.UITheme
 import com.intellij.ide.ui.laf.LafManagerImpl
 import com.intellij.ide.ui.laf.UIThemeBasedLookAndFeelInfo
+import com.intellij.util.io.inputStream
 import io.acari.doki.util.toOptional
+import java.io.InputStreamReader
+import java.nio.charset.StandardCharsets
+import java.nio.file.Files
+import java.nio.file.Paths
 import java.util.*
+import java.util.stream.Collectors
 import javax.swing.UIManager
+
 
 object DokiThemes {
     const val MONIKA_LIGHT = "Monika Light"
 
-    private val themeSet = setOf(
-        "Cleo",
-        "Eleniel",
-        "Neera",
-        "Sanya",
-        "Syrena",
-        "Wyla",
-        "Monika Light",
-        "Natsuki Light",
-        "Sayori Light",
-        "Yuri Light",
-        "Monika Dark",
-        "Natsuki Dark",
-        "Sayori Dark",
-        "Yuri Dark"
-    )
+    private val themeMap: Map<String, UITheme>
+
+    init {
+        val gson = Gson()
+        val path = this.javaClass.classLoader.getResource("themes").path
+        themeMap = Files.walk(Paths.get(path))
+            .filter { it.endsWith(".theme.json") }
+            .map { it.inputStream() }
+            .map {
+                gson.fromJson(
+                    InputStreamReader(it, StandardCharsets.UTF_8),
+                    UITheme::class.java
+                )
+            }
+            .collect(Collectors.toMap(
+                { it.name },
+                { it },
+                { a, _ -> a }
+            ))
+
+        println(themeMap)
+    }
 
     val currentTheme: Optional<DokiTheme>
         get() = processLaf(LafManagerImpl.getInstance().currentLookAndFeel)
@@ -34,7 +49,7 @@ object DokiThemes {
         return currentLaf.toOptional()
             .filter { it is UIThemeBasedLookAndFeelInfo}
             .map { it as UIThemeBasedLookAndFeelInfo }
-            .filter { themeSet.contains(it.name) }
+            .filter { themeMap.containsKey(it.name) }
             .map { DokiTheme(it) }
     }
 }
