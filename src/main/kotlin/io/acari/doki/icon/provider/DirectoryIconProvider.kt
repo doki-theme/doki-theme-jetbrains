e89139a8f4047e79f8994deeb40673e0113dd0b9
DATBOI <alexsimons9999@gmail.com>
2019-12-14 10:43:28 -0600

    Neat! It works

--- src/main/kotlin/io/acari/doki/icon/provider/DirectoryIconProvider.kt
+++ src/main/kotlin/io/acari/doki/icon/provider/DirectoryIconProvider.kt
@@ -1,10 +1,15 @@
 package io.acari.doki.icon.provider
 
+import com.intellij.openapi.util.IconLoader
+import io.acari.doki.icon.provider.associations.Associations
+import io.acari.doki.icon.provider.associations.IconAssociatiorFactory
 import javax.swing.Icon
 
 object DirectoryIconProvider {
-
+  private val associator = IconAssociatiorFactory.create(Associations.DIRECTORY)
   fun getDirectoryIcon(virtualFileInfo: VirtualFileInfo): Icon? {
-    return null
+    return associator.findAssociation(virtualFileInfo)
+      .map { IconLoader.getIcon("/icons/material/folders${it.iconPath}") }
+      .orElseGet { null }
   }
 }
--- src/main/kotlin/io/acari/doki/icon/provider/associations/IconAssociatior.kt
+++ src/main/kotlin/io/acari/doki/icon/provider/associations/IconAssociatior.kt
@@ -1,3 +1,15 @@
 package io.acari.doki.icon.provider.associations
 
-class IconAssociatior(private val iconAssociations: List<IconAssociation>)
+import io.acari.doki.icon.provider.VirtualFileInfo
+import io.acari.doki.util.toOptional
+import java.util.*
+
+class IconAssociatior(private val iconAssociations: List<IconAssociation>) {
+  fun findAssociation(virtualFileInfo: VirtualFileInfo): Optional<IconAssociation> =
+    virtualFileInfo.name.toOptional()
+      .flatMap { fileName ->
+        iconAssociations.stream()
+          .filter { it.associationRegex.matches(fileName) }
+          .findAny()
+      }
+}
