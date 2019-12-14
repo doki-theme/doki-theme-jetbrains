9e66bf181dac243bab09295ee38b2ba14075304d
DATBOI <alexsimons9999@gmail.com>
2019-12-14 10:06:49 -0600

    Added directory association json

--- src/main/kotlin/io/acari/doki/icon/provider/MaterialIconProvider.kt
+++ src/main/kotlin/io/acari/doki/icon/provider/MaterialIconProvider.kt
@@ -1,2 +1,56 @@
 package io.acari.doki.icon.provider
 
+import com.intellij.ide.IconProvider
+import com.intellij.openapi.project.DumbAware
+import com.intellij.openapi.vfs.VirtualFile
+import com.intellij.psi.PsiDirectory
+import com.intellij.psi.PsiElement
+import com.intellij.psi.util.PsiUtilCore
+import io.acari.doki.util.toOptional
+import javax.swing.Icon
+
+
+class MaterialIconProvider : IconProvider(), DumbAware {
+  override fun getIcon(element: PsiElement, flags: Int): Icon? =
+    when (element) {
+      is PsiDirectory -> getDirectoryIcon(element)
+      else -> null
+    }
+
+  private fun getDirectoryIcon(element: PsiDirectory): Icon? {
+    return PsiUtilCore.getVirtualFile(element)
+      .toOptional()
+      //todo: configure enable directory
+      .map { VirtualFileInfo(element, it) }
+      .map { DirectoryIconProvider.getDirectoryIcon(it) }
+      .orElseGet { null }
+  }
+}
+
+class VirtualFileInfo(
+  val psiElement: PsiElement,
+  private val virtualFile: VirtualFile
+) {
+  val name: String?
+    get() = virtualFile.name
+
+  val fileType: String?
+    get() = virtualFile.fileType.name
+
+}
+
+
+object DirectoryIconProvider {
+
+  fun getDirectoryIcon(virtualFileInfo: VirtualFileInfo): Icon? {
+    return null
+  }
+}
+
+class IconAssociation
+
+object IconAssociationFactory {
+  fun create(associationResourcePath: String): IconAssociation {
+    return IconAssociation()
+  }
+}
