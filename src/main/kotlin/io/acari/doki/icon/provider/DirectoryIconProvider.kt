d36323cb8fe25e7e7132273f1635d44d81e6a23b
DATBOI <alexsimons9999@gmail.com>
2019-12-14 10:25:23 -0600

    Pulling things out

---
+++ src/main/kotlin/io/acari/doki/icon/provider/DirectoryIconProvider.kt
@@ -0,0 +1,10 @@
+package io.acari.doki.icon.provider
+
+import javax.swing.Icon
+
+object DirectoryIconProvider {
+
+  fun getDirectoryIcon(virtualFileInfo: VirtualFileInfo): Icon? {
+    return null
+  }
+}
---
+++ src/main/kotlin/io/acari/doki/icon/provider/associations/AssociationModels.kt
@@ -0,0 +1,22 @@
+package io.acari.doki.icon.provider.associations
+
+class IconAssociationDefinition(
+  val name: String,
+  val associationPattern: String,
+  val iconPath: String
+)
+
+data class IconAssociation(
+  val associationRegex: Regex,
+  val iconPath: String
+)
+
+enum class Associations(val fileName: String) {
+  DIRECTORY("directory.association.json"),
+  FILE("file.association.json");
+}
+
+
+class AssociationModels(
+  val associations: List<IconAssociationDefinition>
+)
