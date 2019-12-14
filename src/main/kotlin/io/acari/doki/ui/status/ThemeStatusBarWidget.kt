3bd18f734518ae42ea562ab9a94763b97ce2151d
DATBOI <alexsimons9999@gmail.com>
2019-12-14 08:05:15 -0600

    It works so good!

--- src/main/kotlin/io/acari/doki/ui/status/ThemeStatusBarWidget.kt
+++ src/main/kotlin/io/acari/doki/ui/status/ThemeStatusBarWidget.kt
@@ -1,27 +1,45 @@
 package io.acari.doki.ui.status
 
+import com.intellij.icons.AllIcons
+import com.intellij.ide.ui.LafManagerListener
 import com.intellij.openapi.application.ApplicationManager
 import com.intellij.openapi.application.ModalityState
 import com.intellij.openapi.options.ShowSettingsUtil
 import com.intellij.openapi.project.Project
+import com.intellij.openapi.ui.popup.ListPopup
 import com.intellij.openapi.wm.StatusBar
 import com.intellij.openapi.wm.StatusBarWidget
 import com.intellij.util.Consumer
 import io.acari.doki.settings.ThemeSettings.Companion.THEME_SETTINGS_DISPLAY_NAME
-import java.awt.Component
+import io.acari.doki.themes.ThemeManager
 import java.awt.event.MouseEvent
+import javax.swing.Icon
 
 class ThemeStatusBarWidget(private val project: Project) :
   StatusBarWidget,
-  StatusBarWidget.TextPresentation {
+  StatusBarWidget.MultipleTextValuesPresentation {
   companion object {
     private const val ID = "Doki Theme Status Component"
   }
 
+  private val connect = ApplicationManager.getApplication().messageBus.connect()
+
+  init {
+    connect.subscribe(LafManagerListener.TOPIC, LafManagerListener {
+      if (this::myStatusBar.isInitialized) {
+        myStatusBar.updateWidget(ID)
+      }
+    })
+  }
+
   private lateinit var myStatusBar: StatusBar
 
-  override fun getTooltipText(): String? =
-    "Ravioli"
+  override fun getTooltipText(): String? = "Current Theme"
+
+  override fun getSelectedValue(): String? =
+    ThemeManager.instance.currentTheme
+      .map { it.name }
+      .orElseGet { null }
 
   override fun ID(): String = ID
 
@@ -37,8 +55,10 @@ class ThemeStatusBarWidget(private val project: Project) :
     }
   }
 
-//  override fun getIcon(): Icon =
-//    AllIcons.Nodes.Favorite
+  override fun getIcon(): Icon? =
+    ThemeManager.instance.currentTheme
+      .map { AllIcons.Nodes.Favorite }
+      .orElseGet { null }
 
   override fun getClickConsumer(): Consumer<MouseEvent> = Consumer {
     ApplicationManager.getApplication().invokeLater({
@@ -48,11 +68,5 @@ class ThemeStatusBarWidget(private val project: Project) :
     }, ModalityState.NON_MODAL)
   }
 
-  override fun getAlignment(): Float {
-    return Component.CENTER_ALIGNMENT
-  }
-
-  override fun getText(): String {
-    return "Just Monika"
-  }
+  override fun getPopupStep(): ListPopup? = null
 }
