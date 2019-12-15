25a0e67f536ef8dffb996b4983ad3a29a583594e
DATBOI <alexsimons9999@gmail.com>
2019-12-15 09:34:55 -0600

    Start of the patch patchers

---
+++ src/main/kotlin/io/acari/doki/icon/patcher/MaterialPathPatcher.kt
@@ -0,0 +1,7 @@
+package io.acari.doki.icon.patcher
+
+import com.intellij.openapi.util.IconPathPatcher
+
+class MaterialPathPatcher(
+  private val patcherDefinition: PathPatcherDefinition
+) : IconPathPatcher()
---
+++ src/main/kotlin/io/acari/doki/icon/patcher/MaterialPathPatcherManager.kt
@@ -0,0 +1,33 @@
+package io.acari.doki.icon.patcher
+
+import com.google.gson.Gson
+import com.intellij.util.ResourceUtil
+import java.io.InputStreamReader
+import java.nio.charset.StandardCharsets
+
+object MaterialPathPatcherManager {
+
+}
+
+object MaterialPathPatcherFactory {
+
+  private val gson = Gson()
+
+  val materialPathPatchers: MaterialPathPatchers = try {
+    ResourceUtil.getResource(
+      MaterialPathPatcherFactory::class.java,
+      "/patcher/", "patcher.json"
+    ).openStream().use {
+      val def = gson.fromJson(
+        InputStreamReader(it, StandardCharsets.UTF_8),
+        PathPatchersDefinition::class.java
+      )
+      MaterialPathPatchers(
+        def.glyphs.map { patcherDef -> MaterialPathPatcher(patcherDef) }
+      )
+    }
+  } catch (t: Throwable) {
+    t.printStackTrace()
+    MaterialPathPatchers()
+  }
+}
