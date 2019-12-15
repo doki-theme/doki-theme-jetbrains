ced7096a3b4fae49267dda0dabc00b1838869f30
DATBOI <alexsimons9999@gmail.com>
2019-12-15 10:42:08 -0600

    Refracture material icon directory

--- src/main/kotlin/io/acari/doki/icon/provider/FileIconProvider.kt
+++ src/main/kotlin/io/acari/doki/icon/provider/FileIconProvider.kt
@@ -1,6 +1,7 @@
 package io.acari.doki.icon.provider
 
 import com.intellij.openapi.util.IconLoader
+import io.acari.doki.icon.provider.MaterialIconProvider.Companion.MATERIAL_DIRECTORY
 import io.acari.doki.icon.provider.associations.Associations
 import io.acari.doki.icon.provider.associations.IconAssociatiorFactory
 import javax.swing.Icon
@@ -10,7 +11,7 @@ object FileIconProvider: IconProvider {
       IconAssociatiorFactory.create(Associations.FILE)
   override fun getIcon(virtualFileInfo: VirtualFileInfo): Icon? {
     return associator.findAssociation(virtualFileInfo)
-      .map { IconLoader.getIcon("/icons/material/files${it.iconPath}") }
+      .map { IconLoader.getIcon("$MATERIAL_DIRECTORY/files${it.iconPath}") }
       .orElseGet { null }
   }
 }
