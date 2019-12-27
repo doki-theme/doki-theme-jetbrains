453b84b3d7ec55e976bdefca63e0bb7201af4072
DATBOI <alexsimons9999@gmail.com>
2019-12-27 12:20:49 -0600

    I have not broken anything yet

--- buildSrc/src/main/kotlin/BuildThemes.kt
+++ buildSrc/src/main/kotlin/BuildThemes.kt
@@ -4,9 +4,11 @@ import org.gradle.api.tasks.TaskAction
 import java.io.InputStreamReader
 import java.nio.charset.StandardCharsets
 import java.nio.file.Files
+import java.nio.file.Path
 import java.nio.file.Paths
+import java.util.stream.Collectors
 
-open class BuildThemes: DefaultTask() {
+open class BuildThemes : DefaultTask() {
 
   private val gson = Gson()
 
@@ -16,22 +18,56 @@ open class BuildThemes: DefaultTask() {
   }
 
   @TaskAction
-  fun run(){
+  fun run() {
     val themeDirectory = Paths.get(project.rootDir.absolutePath, "themes")
-    Files.walk(Paths.get(themeDirectory.toString(), "definitions"))
+    val themeTemplates = Files.walk(Paths.get(themeDirectory.toString(), "templates"))
       .filter { !Files.isDirectory(it) }
-      .filter { it.fileName.toString().endsWith(".doki.json") }
+      .filter { it.fileName.toString().endsWith(".template.json") }
       .map { Files.newInputStream(it) }
       .map {
         gson.fromJson(
           InputStreamReader(it, StandardCharsets.UTF_8),
+          ThemeTemplateDefinition::class.java
+        )
+      }.collect(Collectors.toMap({ it.name }, { it }, { a, _ -> a }))
+
+    val themeDefDir = Paths.get(themeDirectory.toString(), "definitions")
+    Files.walk(themeDefDir)
+      .filter { !Files.isDirectory(it) }
+      .filter { it.fileName.toString().endsWith(".doki.json") }
+      .map { it to Files.newInputStream(it) }
+      .map {
+        it.first to gson.fromJson(
+          InputStreamReader(it.second, StandardCharsets.UTF_8),
           DokiBuildThemeDefinition::class.java
         )
-      }.forEach {
+      }
+      .map { constructIntellijTheme(
+        it,
+        themeTemplates,
+        themeDefDir
+      ) }
+      .forEach {
         println(it)
       }
   }
+
+  private fun constructIntellijTheme(
+    dokiBuildThemeDefinition: Pair<Path, DokiBuildThemeDefinition>,
+    themeTemplates: Map<String, ThemeTemplateDefinition>,
+    themeDefDir: Path
+  ): Any {
+    return dokiBuildThemeDefinition.second
+  }
 }
+
+
+data class ThemeTemplateDefinition(
+  val extends: String?,
+  val name: String,
+  val ui: Map<String, Any>,
+  val icons: Map<String, Any>
+)
 
 data class BuildStickers(
   val default: String,
@@ -39,11 +75,14 @@ data class BuildStickers(
 )
 
 data class DokiBuildThemeDefinition(
+  val id: String,
   val name: String,
   val displayName: String?,
   val dark: Boolean,
-  val stickers: BuildStickers,
+  val author: String?,
   val group: String,
+  val editorScheme: Map<String, Any>,
+  val stickers: BuildStickers,
   val colors: Map<String, Any>,
   val ui: Map<String, Any>
 )
