536db7555c3bdbdec865633f34641eab531ee011
DATBOI <alexsimons9999@gmail.com>
2020-03-09 05:28:13 -0500

    Bump version to 7.0.0-ALPHA

--- src/main/kotlin/io/acari/doki/notification/UpdateNotification.kt
+++ src/main/kotlin/io/acari/doki/notification/UpdateNotification.kt
@@ -14,7 +14,7 @@ val UPDATE_MESSAGE: String = """
       Thanks again for downloading <b>The Doki Theme</b>! •‿•<br>
 """.trimIndent()
 
-const val CURRENT_VERSION = "6.2.2"
+const val CURRENT_VERSION = "7.0.0-ALPHA"
 
 object UpdateNotification {
 
