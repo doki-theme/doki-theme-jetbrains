eeb7982f0f0e02f282c0bc81a73429a23c5f2539
DATBOI <alexsimons9999@gmail.com>
2020-03-16 16:29:11 -0500

    Miku Todos dun

---
+++ src/test/kotlin/io/acari/doki/notification/UpdateNotificationTest.kt
@@ -0,0 +1,10 @@
+package io.acari.doki.notification
+
+import org.junit.jupiter.api.Assertions.*
+
+internal class UpdateNotificationTest {
+
+  @org.junit.jupiter.api.Test
+  fun sendMessage() {
+  }
+}
