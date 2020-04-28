c0d3b5b3af2de5fccd14daa153e129e536822456
DATBOI <alexsimons9999@gmail.com>
2020-04-28 10:13:08 -0500

    Bump version to beta

--- src/main/kotlin/io/unthrottled/doki/notification/UpdateNotification.kt
+++ src/main/kotlin/io/unthrottled/doki/notification/UpdateNotification.kt
@@ -21,7 +21,7 @@ val UPDATE_MESSAGE: String = """
       Thanks again for downloading <b>The Doki Theme</b>! •‿•<br>
 """.trimIndent()
 
-const val CURRENT_VERSION = "7.1.2-alpha"
+const val CURRENT_VERSION = "7.1.2-beta"
 
 object UpdateNotification {
 
