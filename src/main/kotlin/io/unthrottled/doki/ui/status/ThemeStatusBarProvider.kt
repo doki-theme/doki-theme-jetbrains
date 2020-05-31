1d77b9b545c3c4304bd6b97f06784e078a21006b
DATBOI <alexsimons9999@gmail.com>
2020-05-31 13:31:56 -0500

    Migrated to the new status bar provider

--- src/main/kotlin/io/unthrottled/doki/ui/status/ThemeStatusBarProvider.kt
+++ src/main/kotlin/io/unthrottled/doki/ui/status/ThemeStatusBarProvider.kt
@@ -1,11 +1,28 @@
 package io.unthrottled.doki.ui.status
 
 import com.intellij.openapi.project.Project
+import com.intellij.openapi.wm.StatusBar
 import com.intellij.openapi.wm.StatusBarWidget
-import com.intellij.openapi.wm.StatusBarWidgetProvider
+import com.intellij.openapi.wm.StatusBarWidgetFactory
 
-class ThemeStatusBarProvider : StatusBarWidgetProvider {
-  override fun getWidget(project: Project): StatusBarWidget {
-    return ThemeStatusBarWidget(project)
+class ThemeStatusBarProvider : StatusBarWidgetFactory {
+
+  override fun getId(): String =
+    "io.unthrottled.doki.ui.status.StatusBarFactory"
+
+  override fun getDisplayName(): String =
+    "Doki Theme Display"
+
+  override fun disposeWidget(widget: StatusBarWidget) {
+    widget.dispose()
   }
+
+  override fun isAvailable(project: Project): Boolean =
+    true
+
+  override fun createWidget(project: Project): StatusBarWidget =
+    ThemeStatusBarWidget(project)
+
+  override fun canBeEnabledOn(statusBar: StatusBar): Boolean =
+    true
 }
