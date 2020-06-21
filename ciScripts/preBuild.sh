ea7d41d6bbdef552dc3e862abd3087e594a04bdc
DATBOI <alexsimons9999@gmail.com>
2020-06-21 06:17:57 -0500

    Added prebuild step to produce changelog in plugin

--- ciScripts/preBuild.sh
+++ ciScripts/preBuild.sh
@@ -6,3 +6,4 @@ git pull origin master
 rm -rf ~/workspace/doki-theme-jetbrains/src/main/resources/club_members/
 rm -rf ~/workspace/doki-theme-jetbrains/src/main/resources/themes/
 rm -rf ~/workspace/doki-theme-jetbrains/src/main/resources/stickers/
+./gradlew markdownToHtml
