e638951ae8709d3e6dc433e3852181796cf3f74e
DATBOI <alexsimons9999@gmail.com>
2020-09-12 15:44:55 -0500

    Style pretty"

--- src/main/kotlin/io/unthrottled/doki/promotions/MotivatorPromotion.kt
+++ src/main/kotlin/io/unthrottled/doki/promotions/MotivatorPromotion.kt
@@ -8,6 +8,7 @@ import com.intellij.util.ui.UIUtil
 import io.unthrottled.doki.icon.DokiIcons
 import io.unthrottled.doki.themes.DokiTheme
 import io.unthrottled.doki.util.toHexString
+import java.awt.Dimension
 import java.awt.Window
 import javax.swing.JComponent
 import javax.swing.JEditorPane
@@ -60,9 +61,9 @@ class MotivatorPromotion(
           <title>Motivator</title>
       </head>
       <body>
-      <div style='text-align: center'><img src="https://doki.assets.unthrottled.io/misc/motivator_logo.png" alt='Motivator Plugin Logo'> </div>
+      <div style='text-align: center; margin-top: 8px'><img src="https://doki.assets.unthrottled.io/misc/motivator_logo.png" alt='Motivator Plugin Logo'> </div>
       <h2 style='text-align: center; color: $accentHex'>Your new virtual IDE companion!</h2>
-      <div style='margin: 8px 0 0'>
+      <div style='margin: 8px 0 0 115px'>
         <p>
           The <a href='https://plugins.jetbrains.com/plugin/13381-waifu-motivator'>Waifu Motivator Plugin</a>
           gives you a virtual companion. <br/> Your companion will interact with you as code is being built.<br/>
@@ -75,6 +76,7 @@ class MotivatorPromotion(
       </body>
       </html>
     """.trimIndent()
+    pane.preferredSize = Dimension(pane.preferredSize.width + 150, pane.preferredSize.height)
     pane.addHyperlinkListener {
       if (it.eventType == HyperlinkEvent.EventType.ACTIVATED) {
           BrowserUtil.browse(it.url)
