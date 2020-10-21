7b2f3be852151c1143b1c5d0415c1c109f4c6e24
DATBOI <alexsimons9999@gmail.com>
2020-10-21 04:54:50 -0500

    Updated changelog and bumped version to v11.1.0

--- src/main/kotlin/io/unthrottled/doki/notification/UpdateNotification.kt
+++ src/main/kotlin/io/unthrottled/doki/notification/UpdateNotification.kt
@@ -21,14 +21,8 @@ import java.awt.Point
 val UPDATE_MESSAGE: String = """
       What's New?<br>
       <ul>
-        <li>5 New Themes!!
-            <ul>
-              <li>Fate - Ishtar (Light/Dark)</li>
-              <li>Fate - Tohsaka Rin (Dark)</li>
-              <li>Gate - Rory Mercury (Dark)</li>
-              <li>Konosuba - Aqua (Dark)</li>
-            </ul>
-        </li>
+        <li>Better 2020.3 Support</li>
+        <li>Enhanced Rory's & Ishtar's dark theme.</li>
       </ul>
       Please see the <a href="https://github.com/doki-theme/doki-theme-jetbrains/blob/master/changelog/CHANGELOG.md">changelog</a> for more details.
       <br><br>
