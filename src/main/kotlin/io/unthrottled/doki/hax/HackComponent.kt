2d9afc27d54859d697aae8a1296e3e4130010d89
DATBOI <alexsimons9999@gmail.com>
2020-10-23 05:45:18 -0500

    Less trying, more running with safety

--- src/main/kotlin/io/unthrottled/doki/hax/HackComponent.kt
+++ src/main/kotlin/io/unthrottled/doki/hax/HackComponent.kt
@@ -30,6 +30,7 @@ import io.unthrottled.doki.hax.FeildHacker.setFinalStatic
 import io.unthrottled.doki.stickers.impl.DOKI_BACKGROUND_PROP
 import io.unthrottled.doki.stickers.impl.DOKI_STICKER_PROP
 import io.unthrottled.doki.ui.TitlePaneUI.Companion.LOL_NOPE
+import io.unthrottled.doki.util.runSafely
 import javassist.*
 import javassist.expr.ExprEditor
 import javassist.expr.MethodCall
@@ -68,15 +69,15 @@ object HackComponent : Disposable {
   }
 
   private fun hackLivePreview() {
-    try {
+    runSafely({
       val naughtySelectionColor = IdeTooltipManager::class.java.getDeclaredField("GRAPHITE_COLOR")
       val namedColor = JBColor.namedColor("TextPane.background", 0xf2f2f2)
       setFinalStatic(naughtySelectionColor, namedColor)
-    } catch (e: Throwable) {
+    }) {
       log.warn("Unable to hackLivePreview for reasons.")
     }
 
-    try {
+    runSafely({
       val cp = ClassPool(true)
       cp.insertClassPath(ClassClassPath(Class.forName("com.intellij.find.impl.livePreview.SearchResults")))
       val ctClass = cp.get("com.intellij.find.impl.livePreview.ReplacementView")
@@ -89,13 +90,13 @@ object HackComponent : Disposable {
         }
       })
       ctClass.toClass()
-    } catch (e: Exception) {
+    }) {
       log.warn("Unable to hackLivePreview for reasons.")
     }
   }
 
   private fun hackWelcomeScreen() {
-    try {
+    runSafely({
       val cp = ClassPool(true)
       cp.insertClassPath(ClassClassPath(Class.forName("com.intellij.openapi.wm.impl.welcomeScreen.EditProjectGroupAction")))
       val ctClass = cp.get("com.intellij.openapi.wm.impl.welcomeScreen.FlatWelcomeFrame")
@@ -110,23 +111,23 @@ object HackComponent : Disposable {
         }
       })
       ctClass.toClass()
-    } catch (e: Exception) {
+    }) {
       log.warn("Unable to hackWelcomeScreen for reasons.")
     }
   }
 
   private fun hackBookmarkBorder() {
-    try {
+    runSafely({
       val naughtySelectionColor = MasterDetailPopupBuilder::class.java.getDeclaredField("BORDER_COLOR")
       val namedColor = JBColor.namedColor("Borders.color", Gray._135)
       setFinalStatic(naughtySelectionColor, namedColor)
-    } catch (e: Exception) {
+    }) {
       log.warn("Unable to hackBookMarkBorder  for reasons.")
     }
   }
 
   private fun hackSheetMessage() {
-    try {
+    runSafely({
       val cp = ClassPool(true)
       cp.insertClassPath(ClassClassPath(JBMacMessages::class.java))
       val ctClass = cp.get("com.intellij.ui.messages.SheetMessage")
@@ -141,13 +142,13 @@ object HackComponent : Disposable {
           })
         }
       ctClass.toClass()
-    } catch (e: Exception) {
+    }) {
       log.warn("Unable to hackSheetMessage for reasons.")
     }
   }
 
   private fun hackSheetWindow() {
-    try {
+    runSafely({
       val cp = ClassPool(true)
       cp.insertClassPath(ClassClassPath(JBMacMessages::class.java))
       val ctClass = cp.get("com.intellij.ui.messages.SheetController")
@@ -172,13 +173,13 @@ object HackComponent : Disposable {
           classDude.toClass()
         }
       ctClass.toClass()
-    } catch (e: Exception) {
+    }) {
       log.warn("Unable to hackSheetWindow for reasons.")
     }
   }
 
   private fun hackParameterInfoBackground() {
-    try {
+    runSafely({
       val cp = ClassPool(true)
       cp.insertClassPath(ClassClassPath(HintUtil::class.java))
       val ctClass = cp.get("com.intellij.codeInsight.hint.ParameterInfoComponent\$OneElementComponent")
@@ -196,7 +197,7 @@ object HackComponent : Disposable {
       val bitchassClass = cp.get("com.intellij.codeInsight.hint.ParameterInfoComponent\$OneLineComponent")
       bitchassClass.getDeclaredMethods("doSetup").forEach(hackBackground)
       bitchassClass.toClass()
-    } catch (e: Exception) {
+    }) {
       log.warn("Unable to hackParameterInfoBackground for reasons.")
     }
   }
@@ -206,7 +207,7 @@ object HackComponent : Disposable {
   }
 
   private fun hackLiveIndicator() {
-    try {
+    runSafely({
       val cp = ClassPool(true)
       cp.insertClassPath(ClassClassPath(ProcessProxy::class.java))
       val ctClass = cp.get("com.intellij.execution.runners.ExecutionUtil")
@@ -221,7 +222,7 @@ object HackComponent : Disposable {
         }
       })
       ctClass.toClass()
-    } catch (e: Exception) {
+    }) {
       log.warn("Unable to hackLiveIndicator for reasons.")
     }
   }
@@ -231,7 +232,7 @@ object HackComponent : Disposable {
   }
 
   private fun hackActionModel() {
-    try {
+    runSafely({
       val cp = ClassPool(true)
       cp.insertClassPath(ClassClassPath(CustomMatcherModel::class.java))
       val ctClass = cp.get("com.intellij.ide.util.gotoByName.GotoActionModel\$GotoActionListCellRenderer")
@@ -246,13 +247,13 @@ object HackComponent : Disposable {
         }
       })
       ctClass.toClass()
-    } catch (e: Exception) {
+    }) {
       log.warn("Unable to hackActionModel for reasons.")
     }
   }
 
   private fun hackToolWindowDecorator() {
-    try {
+    runSafely({
       val cp = ClassPool(true)
       cp.insertClassPath(ClassClassPath(TitleInfoProvider::class.java))
       val ctClass = cp.get("com.intellij.openapi.wm.impl.ToolWindowHeader")
@@ -267,7 +268,7 @@ object HackComponent : Disposable {
         }
       })
       ctClass.toClass()
-    } catch (e: Exception) {
+    }) {
       log.warn("Unable to hackToolWindowDecorator for reasons.")
     }
   }
@@ -285,7 +286,7 @@ object HackComponent : Disposable {
   }
 
   private fun hackLineStatusColor() {
-    try {
+    runSafely({
       val cp = ClassPool(true)
       cp.insertClassPath(ClassClassPath(LineStatusMarkerRenderer::class.java))
       val bitchassClass = cp.get("com.intellij.openapi.vcs.ex.LineStatusMarkerPopupRenderer\$PopupPanel")
@@ -299,13 +300,13 @@ object HackComponent : Disposable {
         })
       }
       bitchassClass.toClass()
-    } catch (e: Exception) {
+    }) {
       log.warn("Unable to hackStatusLineColor for reasons.")
     }
   }
 
   private fun hackPopupBorder() {
-    try {
+    runSafely({
       val cp = ClassPool(true)
       cp.insertClassPath(ClassClassPath(TooltipRenderer::class.java))
       val ctClass = cp.get("com.intellij.codeInsight.hint.HintManagerImpl")
@@ -318,11 +319,11 @@ object HackComponent : Disposable {
         }
       })
       ctClass.toClass()
-    } catch (e: Exception) {
+    }) {
       log.warn("Unable to hackPopupBorder for reasons.")
     }
 
-    try {
+    runSafely({
       val cp = ClassPool(true)
       cp.insertClassPath(ClassClassPath(DirectoryFormattingOptions::class.java))
       val ctClass = cp.get("com.intellij.codeInsight.actions.FileInEditorProcessor\$FormattedMessageBuilder")
@@ -335,17 +336,17 @@ object HackComponent : Disposable {
         }
       })
       ctClass.toClass()
-    } catch (e: Exception) {
+    }) {
       log.warn("Unable to hackPopupBorder for reasons.")
     }
   }
 
   private fun hackTipBorder() {
-    try {
+    runSafely({
       val naughtySelectionColor = TipPanel::class.java.getDeclaredField("DIVIDER_COLOR")
       val namedColor = JBColor.namedColor("Borders.color", 0xf2f2f2)
       setFinalStatic(naughtySelectionColor, namedColor)
-    } catch (e: Throwable) {
+    }) {
       log.warn("Unable to hackTipBorder for reasons.")
     }
   }
@@ -362,17 +363,17 @@ object HackComponent : Disposable {
 
   private fun hackTestResults() {
     val switcherClass = ColorProgressBar::class.java
-    try {
+    runSafely({
       val naughtySelectionColor = switcherClass.getDeclaredField("YELLOW")
       val namedColor = JBColor.namedColor("ColorProgress.bar.yellow", JBColor(Color(0xa67a21), Color(0x91703a)))
       setFinalStatic(naughtySelectionColor, namedColor)
-    } catch (e: Throwable) {
+    }) {
       log.warn("Unable to hackTestResults for reasons.")
     }
   }
 
   private fun hackTitleFrame() {
-    try {
+    runSafely({
       val cp = ClassPool(true)
       cp.insertClassPath(ClassClassPath(Class.forName("com.intellij.openapi.wm.impl.welcomeScreen.CardActionsPanel")))
       val ctClass = cp.get("com.intellij.openapi.wm.impl.welcomeScreen.FlatWelcomeFrame\$FlatWelcomeScreen")
@@ -391,61 +392,61 @@ object HackComponent : Disposable {
         }
       })
       ctClass.toClass()
-    } catch (e: Exception) {
+    }) {
       log.warn("Unable to hackTitleFrame for reasons.")
     }
   }
 
   private fun hackColors() {
-    try {
+    runSafely({
       val naughtySelectionColor = JBColor::class.java.getDeclaredField("GRAY")
       val namedColor = JBColor.namedColor("Label.infoForeground", Gray._128)
       setFinalStatic(naughtySelectionColor, namedColor)
-    } catch (e: Throwable) {
+    }) {
       log.warn("Unable to hackColors for reasons.")
     }
   }
 
   private fun hackSdkComboBox() {
-    try {
+    runSafely({
       val naughtySelectionColor = SimpleTextAttributes::class.java.getDeclaredField("SIMPLE_CELL_ATTRIBUTES")
       val namedColor = SimpleTextAttributes(SimpleTextAttributes.STYLE_PLAIN, UIUtil.getLabelForeground())
       setFinalStatic(naughtySelectionColor, namedColor)
-    } catch (e: Throwable) {
+    }) {
       log.warn("Unable to hackSdkComboBox for reasons.")
     }
   }
 
   private fun hackSwitcher() {
-    try {
+    runSafely({
       val naughtySelectionColor = SimpleTextAttributes::class.java.getDeclaredField("GRAY_ATTRIBUTES")
       val namedColor = SimpleTextAttributes(SimpleTextAttributes.STYLE_PLAIN, UIUtil.getContextHelpForeground())
       setFinalStatic(naughtySelectionColor, namedColor)
-    } catch (e: Throwable) {
+    }) {
       log.warn("Unable to hackSwitcher for reasons.")
     }
   }
 
   private fun hackDebuggerAttributes() {
-    try {
+    runSafely({
       val naughtySelectionColor = XDebuggerUIConstants::class.java.getDeclaredField("TYPE_ATTRIBUTES")
       val namedColor = SimpleTextAttributes(SimpleTextAttributes.STYLE_PLAIN, UIUtil.getContextHelpForeground())
       setFinalStatic(naughtySelectionColor, namedColor)
-    } catch (e: Throwable) {
+    }) {
       log.warn("Unable to hackDebuggerAttributes for reasons.")
     }
-    try {
+    runSafely({
       val naughtySelectionColor = SimpleTextAttributes::class.java.getDeclaredField("REGULAR_ITALIC_ATTRIBUTES")
       val namedColor = SimpleTextAttributes(SimpleTextAttributes.STYLE_ITALIC, UIUtil.getLabelForeground())
       setFinalStatic(naughtySelectionColor, namedColor)
-    } catch (e: Throwable) {
+    }) {
       log.warn("Unable to hackDebuggerAttributes for reasons.")
     }
-    try {
+    runSafely({
       val naughtySelectionColor = SimpleTextAttributes::class.java.getDeclaredField("GRAY_ITALIC_ATTRIBUTES")
       val namedColor = SimpleTextAttributes(SimpleTextAttributes.STYLE_ITALIC, UIUtil.getContextHelpForeground())
       setFinalStatic(naughtySelectionColor, namedColor)
-    } catch (e: Throwable) {
+    }) {
       log.warn("Unable to hackDebuggerAttributes for reasons.")
     }
   }
@@ -456,11 +457,11 @@ object HackComponent : Disposable {
 
   private fun hackSwitcherSelection() {
     val switcherClass = Switcher::class.java
-    try {
+    runSafely({
       val naughtySelectionColor = switcherClass.getDeclaredField("ON_MOUSE_OVER_BG_COLOR")
       val namedColor = JBColor.namedColor("List.selectionBackground", 0xf2f2f2)
       setFinalStatic(naughtySelectionColor, namedColor)
-    } catch (e: Throwable) {
+    }) {
       log.warn("Unable to hackSwitcherSelection for reasons.")
     }
   }
@@ -472,7 +473,7 @@ object HackComponent : Disposable {
   }
 
   private fun hackEditorBorder() {
-    try {
+    runSafely({
       val cp = ClassPool(true)
       cp.insertClassPath(ClassClassPath(EditorHistoryManager::class.java))
       val ctClass = cp.get("com.intellij.openapi.fileEditor.impl.EditorsSplitters")
@@ -487,13 +488,13 @@ object HackComponent : Disposable {
         }
       })
       ctClass.toClass()
-    } catch (e: Exception) {
+    }) {
       log.warn("Unable to hackEditorBorder for reasons.")
     }
   }
 
   private fun hackSwitcherBorder() {
-    try {
+    runSafely({
       val cp = ClassPool(true)
       cp.insertClassPath(ClassClassPath(Switcher::class.java))
       val ctClass = cp.get("com.intellij.ide.actions.SwitcherToolWindowsListRenderer")
@@ -506,13 +507,13 @@ object HackComponent : Disposable {
         }
       })
       ctClass.toClass()
-    } catch (e: Exception) {
+    }) {
       log.warn("Unable to hackSwitcherBorder for reasons.")
     }
   }
 
   private fun hackFindInPath() {
-    try {
+    runSafely({
       val cp = ClassPool(true)
       cp.insertClassPath(ClassClassPath(ActionMenu::class.java))
       val ctClass = cp.get("com.intellij.openapi.actionSystem.impl.ActionToolbarImpl")
@@ -525,7 +526,7 @@ object HackComponent : Disposable {
         }
       })
       ctClass.toClass()
-    } catch (e: Exception) {
+    }) {
       log.warn("Unable to hackFindInPath for reasons.")
     }
   }
@@ -535,7 +536,7 @@ object HackComponent : Disposable {
   }
 
   private fun hackPluginWindow() {
-    try {
+    runSafely({
       val cp = ClassPool(true)
       cp.insertClassPath(ClassClassPath(PluginLogo::class.java))
       val ctClass = cp.get("com.intellij.ide.plugins.newui.PluginDetailsPageComponent")
@@ -548,7 +549,7 @@ object HackComponent : Disposable {
         }
       })
       ctClass.toClass()
-    } catch (e: Exception) {
+    }) {
       log.warn("Unable to hackPluginWindow for reasons.")
     }
   }
@@ -559,30 +560,30 @@ object HackComponent : Disposable {
   }
 
   private fun hackTipDialog() {
-    try {
+    runSafely({
       val cp = ClassPool(true)
       cp.insertClassPath(ClassClassPath(ExportToFileUtil::class.java))
       val ctClass = cp.get("com.intellij.ide.util.TipDialog")
       hackTip(ctClass)
-    } catch (e: Throwable) {
+    }) {
       log.warn("Unable to hackTipDialog for reasons.")
     }
   }
 
   private fun hackTip(ctClass: CtClass) {
-    try {
+    runSafely({
       val declaredConstructor = ctClass.constructors
       declaredConstructor.forEach {
         it.insertAfter("this.setTitle(\"Monika's Writing Tip of the Day\");")
       }
       ctClass.toClass()
-    } catch (e: Throwable) {
+    }) {
       log.warn("Unable to hackTip for reasons.")
     }
   }
 
   private fun hackTipPanel() {
-    try {
+    runSafely({
       val cp = ClassPool(true)
       cp.insertClassPath(ClassClassPath(ChooseElementsDialog::class.java))
       val ctClass = cp.get("com.intellij.ide.util.TipPanel")
@@ -596,7 +597,7 @@ object HackComponent : Disposable {
         }
       })
       ctClass.toClass()
-    } catch (e: Exception) {
+    }) {
       log.warn("Unable to hackTipPanel for reasons.")
     }
   }
@@ -614,7 +615,7 @@ object HackComponent : Disposable {
    * but also allows prevents the background image from staying after installation.
    */
   private fun hackBackgroundPaintingComponent() {
-    try {
+    runSafely({
       val cp = ClassPool(true)
       cp.insertClassPath(ClassClassPath(EditorComposite::class.java))
       val ctClass2 = cp.get("com.intellij.openapi.wm.impl.IdeBackgroundUtil")
@@ -682,7 +683,7 @@ object HackComponent : Disposable {
       })
 
       ctClass2.toClass()
-    } catch (e: Exception) {
+    }) {
       log.warn("Unable to hackBackgroundPaintingComponent for reasons.")
     }
   }
