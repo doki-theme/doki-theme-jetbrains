1025bb9880d4ba2d1d92f575a40d02078acb049d
DATBOI <alexsimons9999@gmail.com>
2021-04-01 17:08:01 -0500

    Handling Ria's rename

--- src/main/kotlin/io/unthrottled/doki/legacy/LegacyMigration.kt
+++ src/main/kotlin/io/unthrottled/doki/legacy/LegacyMigration.kt
@@ -1,6 +1,26 @@
 package io.unthrottled.doki.legacy
 
+import io.unthrottled.doki.settings.actors.setDokiTheme
+import io.unthrottled.doki.themes.ThemeManager
+import io.unthrottled.doki.util.toOptional
+
 object LegacyMigration {
   fun migrateIfNecessary() {
+    handleThemeRenames()
+  }
+
+  private fun handleThemeRenames() {
+    ThemeManager.instance.currentTheme
+      .filter {
+        it.id == "c5e92ad9-2fa0-491e-b92a-48ab92d13597"
+      }
+      .ifPresent { renamedTheme ->
+        setDokiTheme(
+          ThemeManager.instance.allThemes.first {
+            renamedTheme.id != it.id
+          }.toOptional()
+        )
+        setDokiTheme(renamedTheme.toOptional())
+      }
   }
 }
