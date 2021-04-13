30a6d4e3606cfd9ac7664411d26b1c1deeacb356
DATBOI <alexsimons9999@gmail.com>
2021-04-13 17:45:24 -0500

    Fixing Promotion Logic

--- src/main/kotlin/io/unthrottled/doki/service/PluginService.kt
+++ src/main/kotlin/io/unthrottled/doki/service/PluginService.kt
@@ -1,8 +1,9 @@
 package io.unthrottled.doki.service
 
 import com.intellij.ide.plugins.PluginManagerCore
+import com.intellij.ide.plugins.marketplace.MarketplaceRequests
 import com.intellij.openapi.extensions.PluginId
-import io.unthrottled.doki.util.toOptional
+import java.util.Collections
 
 const val MOTIVATOR_PLUGIN_ID = "zd.zero.waifu-motivator-plugin"
 const val AMII_PLUGIN_ID = "io.unthrottled.amii"
@@ -17,8 +18,13 @@ object PluginService {
     PluginId.getId(AMII_PLUGIN_ID)
   )
 
-  fun canAmiiBeInstalled(): Boolean =
-    PluginManagerCore.getPlugin(PluginId.getId(AMII_PLUGIN_ID)).toOptional()
-      .filter { PluginManagerCore.isCompatible(it) }
-      .isPresent
+  fun canAmiiBeInstalled(): Boolean {
+    val ids = AMII_PLUGIN_ID
+    val pluginId = PluginId.getId(ids)
+    return MarketplaceRequests.getInstance().loadLastCompatiblePluginDescriptors(
+      Collections.singletonList(ids)
+    ).firstOrNull { pluginNode ->
+      pluginNode.pluginId == pluginId
+    } != null
+  }
 }
