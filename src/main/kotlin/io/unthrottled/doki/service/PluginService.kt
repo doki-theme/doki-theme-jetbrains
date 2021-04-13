55faae6640dea23782a496e54307e2f9786a83b1
DATBOI <alexsimons9999@gmail.com>
2021-04-13 17:54:39 -0500

    Not calling the network on the AWT thread

--- src/main/kotlin/io/unthrottled/doki/service/PluginService.kt
+++ src/main/kotlin/io/unthrottled/doki/service/PluginService.kt
@@ -2,8 +2,10 @@ package io.unthrottled.doki.service
 
 import com.intellij.ide.plugins.PluginManagerCore
 import com.intellij.ide.plugins.marketplace.MarketplaceRequests
+import com.intellij.openapi.application.ApplicationManager
 import com.intellij.openapi.extensions.PluginId
 import java.util.Collections
+import java.util.concurrent.Callable
 
 const val MOTIVATOR_PLUGIN_ID = "zd.zero.waifu-motivator-plugin"
 const val AMII_PLUGIN_ID = "io.unthrottled.amii"
@@ -19,12 +21,14 @@ object PluginService {
   )
 
   fun canAmiiBeInstalled(): Boolean {
-    val ids = AMII_PLUGIN_ID
-    val pluginId = PluginId.getId(ids)
-    return MarketplaceRequests.getInstance().loadLastCompatiblePluginDescriptors(
-      Collections.singletonList(ids)
-    ).firstOrNull { pluginNode ->
-      pluginNode.pluginId == pluginId
-    } != null
+    return ApplicationManager.getApplication().executeOnPooledThread(Callable {
+      val ids = AMII_PLUGIN_ID
+      val pluginId = PluginId.getId(ids)
+      MarketplaceRequests.getInstance().loadLastCompatiblePluginDescriptors(
+        Collections.singletonList(ids)
+      ).firstOrNull { pluginNode ->
+        pluginNode.pluginId == pluginId
+      } != null
+    }).get()
   }
 }
