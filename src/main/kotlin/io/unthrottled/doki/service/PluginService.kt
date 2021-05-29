30767d734efb98ea4fa8f37cb72f8dfc4c8f993e
DATBOI <alexsimons9999@gmail.com>
2021-05-29 16:48:54 -0500

    Did I get all the compatability issues or the plugin service

--- src/main/kotlin/io/unthrottled/doki/service/PluginService.kt
+++ src/main/kotlin/io/unthrottled/doki/service/PluginService.kt
@@ -7,7 +7,6 @@ import com.intellij.ide.plugins.marketplace.IdeCompatibleUpdate
 import com.intellij.openapi.application.ApplicationManager
 import com.intellij.openapi.application.impl.ApplicationInfoImpl
 import com.intellij.openapi.extensions.PluginId
-import com.intellij.openapi.util.BuildNumber
 import com.intellij.util.Urls
 import com.intellij.util.io.HttpRequests
 import io.unthrottled.doki.util.Logging
@@ -29,7 +28,7 @@ object PluginService : Logging {
     PluginId.getId(AMII_PLUGIN_ID)
   )
 
-  private val PLUGIN_MANAGER_URL = ApplicationInfoImpl.getShadowInstanceImpl()
+  private val PLUGIN_MANAGER_URL = ApplicationInfoImpl.getInstanceEx()
     .pluginManagerUrl
     .trimEnd('/')
 
@@ -38,28 +37,22 @@ object PluginService : Logging {
   private data class CompatibleUpdateRequest(
     val build: String,
     val pluginXMLIds: List<String>,
-  ) {
-
-    @JvmOverloads
-    constructor(
-      pluginIds: Set<PluginId>,
-      buildNumber: BuildNumber? = null,
-    ) : this(
-      ApplicationInfoImpl.orFromPluginsCompatibleBuild(buildNumber),
-      pluginIds.map { it.idString },
-    )
-  }
+  )
 
   private fun getLastCompatiblePluginUpdate(
-    ids: Set<PluginId>,
-    buildNumber: BuildNumber? = null,
+    ids: Set<PluginId>
   ): List<IdeCompatibleUpdate> {
     return try {
       if (ids.isEmpty()) {
         return emptyList()
       }
 
-      val data = objectMapper.writeValueAsString(CompatibleUpdateRequest(ids, buildNumber))
+      val data = objectMapper.writeValueAsString(
+        CompatibleUpdateRequest(
+          ApplicationInfoImpl.getInstanceEx()
+            .build.asString(), ids.map { it.idString }
+        )
+      )
       HttpRequests
         .post(Urls.newFromEncoded(COMPATIBLE_UPDATE_URL).toExternalForm(), HttpRequests.JSON_CONTENT_TYPE)
         .productNameAsUserAgent()
