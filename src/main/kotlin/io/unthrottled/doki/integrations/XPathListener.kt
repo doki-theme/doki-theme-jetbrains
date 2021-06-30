90e88b8a9834c47f23dd923829f521299de88c83
DATBOI <alexsimons9999@gmail.com>
2021-06-30 08:03:01 -0500

    Enhanced XPath colors

--- src/main/kotlin/io/unthrottled/doki/integrations/XPathListener.kt
+++ src/main/kotlin/io/unthrottled/doki/integrations/XPathListener.kt
@@ -2,25 +2,34 @@ package io.unthrottled.doki.integrations
 
 import com.intellij.ide.ui.LafManager
 import com.intellij.ide.ui.LafManagerListener
+import com.intellij.openapi.editor.colors.EditorColors
+import com.intellij.openapi.editor.colors.EditorColors.IDENTIFIER_UNDER_CARET_ATTRIBUTES
+import com.intellij.openapi.editor.colors.EditorColorsManager
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.startup.StartupActivity
 import io.unthrottled.doki.themes.ThemeManager
 import org.intellij.plugins.xpathView.Config
 import org.intellij.plugins.xpathView.XPathAppComponent
 
-class XPathListener: LafManagerListener, StartupActivity {
+class XPathListener : LafManagerListener, StartupActivity {
   override fun lookAndFeelChanged(source: LafManager) {
     installColors()
   }
 
   private fun installColors() {
     ThemeManager.instance.currentTheme.ifPresentOrElse({
-      XPathAppComponent.getInstance().config.attributes.backgroundColor = it.selectionBackground
-      XPathAppComponent.getInstance().config.attributes.foregroundColor = it.selectionForeground
+      val schemeForCurrentUITheme = EditorColorsManager.getInstance().schemeForCurrentUITheme
+      XPathAppComponent.getInstance().config.attributes.backgroundColor = schemeForCurrentUITheme.getColor(
+        EditorColors.SELECTION_BACKGROUND_COLOR
+      )
+      XPathAppComponent.getInstance().config.attributes.foregroundColor = schemeForCurrentUITheme.getColor(
+        EditorColors.SELECTION_FOREGROUND_COLOR
+      )
 
-      XPathAppComponent.getInstance().config.contextAttributes.backgroundColor = it.identifierHighlight
+      XPathAppComponent.getInstance().config.contextAttributes.backgroundColor =
+        IDENTIFIER_UNDER_CARET_ATTRIBUTES.defaultAttributes.backgroundColor
 
-      }) {
+    }) {
       val config = Config()
       XPathAppComponent.getInstance().config.attributes.backgroundColor = config.attributes.backgroundColor
       XPathAppComponent.getInstance().config.attributes.backgroundColor = config.attributes.foregroundColor
@@ -29,7 +38,7 @@ class XPathListener: LafManagerListener, StartupActivity {
         config.contextAttributes.backgroundColor
       XPathAppComponent.getInstance().config.contextAttributes.foregroundColor =
         config.contextAttributes.foregroundColor
-      }
+    }
   }
 
   override fun runActivity(project: Project) {
