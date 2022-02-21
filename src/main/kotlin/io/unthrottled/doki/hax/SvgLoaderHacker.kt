57b4d521ae210ffee89c42bd12ee5d6aca933288
DATBOI <alexsimons9999@gmail.com>
2022-02-21 11:22:42 -0600

    Stopping patching recursion.

--- src/main/kotlin/io/unthrottled/doki/hax/SvgLoaderHacker.kt
+++ src/main/kotlin/io/unthrottled/doki/hax/SvgLoaderHacker.kt
@@ -6,32 +6,24 @@ import io.unthrottled.doki.themes.DokiTheme
 import java.net.URL
 import java.util.Optional
 
-typealias SVGL = SVGLoader
 typealias PatcherProvider = SVGLoader.SvgElementColorPatcherProvider
 typealias Patcher = SVGLoader.SvgElementColorPatcher
 
 object SvgLoaderHacker {
 
-  private lateinit var otherColorPatcher: PatcherProvider
 
   /**
    * Enables the ability to have more than one color patcher.
    */
   fun setSVGColorPatcher(dokiTheme: DokiTheme) {
-    SVGLoader.setColorPatcherProvider(
-      collectOtherPatcher()
-        .map { patcher ->
-          ColorPatcher(patcher, dokiTheme)
-        }
-        .orElseGet {
-          ColorPatcher(
-            object : PatcherProvider {
-              override fun forURL(url: URL?): SVGLoader.SvgElementColorPatcher? = null
-            },
-            dokiTheme
-          )
-        }
-    )
+    collectOtherPatcher()
+      .ifPresent { otherPatcher ->
+        ColorPatcher.setOtherPatcher(otherPatcher)
+      }
+
+    ColorPatcher.setDokiTheme(dokiTheme)
+
+    SVGLoader.setColorPatcherProvider(ColorPatcher)
   }
 
   private fun collectOtherPatcher(): Optional<PatcherProvider> =
@@ -47,13 +39,7 @@ object SvgLoaderHacker {
       .filter { it !is ColorPatcher }
       .map {
         val otherPatcher = it as PatcherProvider
-        otherColorPatcher = otherPatcher
         otherPatcher
       }
-      .map { Optional.of(it) }
-      .orElseGet { useFallBackPatcher() }
 
-  private fun useFallBackPatcher(): Optional<PatcherProvider> =
-    if (this::otherColorPatcher.isInitialized) Optional.of(otherColorPatcher)
-    else Optional.empty()
 }
--- src/main/kotlin/io/unthrottled/doki/icon/ColorPatcher.kt
+++ src/main/kotlin/io/unthrottled/doki/icon/ColorPatcher.kt
@@ -1,7 +1,12 @@
 package io.unthrottled.doki.icon
 
+import com.google.common.cache.Cache
+import com.google.common.cache.CacheBuilder
+import com.intellij.ide.ui.LafManager
+import com.intellij.ide.ui.laf.UIThemeBasedLookAndFeelInfo
 import com.intellij.ui.ColorUtil
 import com.intellij.ui.JBColor.namedColor
+import com.intellij.util.SVGLoader
 import com.intellij.util.io.DigestUtil
 import io.unthrottled.doki.hax.Patcher
 import io.unthrottled.doki.hax.PatcherProvider
@@ -10,11 +15,13 @@ import io.unthrottled.doki.themes.ThemeManager
 import io.unthrottled.doki.util.runSafely
 import io.unthrottled.doki.util.runSafelyWithResult
 import io.unthrottled.doki.util.toHexString
+import io.unthrottled.doki.util.toOptional
 import org.w3c.dom.Element
 import java.awt.Color
 import java.net.URL
+import java.time.Duration
 
-object NoOptPatcher: Patcher {
+object NoOptPatcher : Patcher {
   override fun patchColors(svg: Element) {}
   val byteArray = ByteArray(0)
   override fun digest(): ByteArray {
@@ -22,68 +29,128 @@ object NoOptPatcher: Patcher {
   }
 }
 
-class ColorPatcher(
-  private val otherColorPatcherProvider: PatcherProvider,
-  dokiTheme: DokiTheme
-) : PatcherProvider {
+val emptyByteArray = ByteArray(0)
 
-  companion object {
-    private val emptyByteArray = ByteArray(0)
+val noOptPatcherProvider = object : PatcherProvider {
+  override fun forURL(url: URL?): SVGLoader.SvgElementColorPatcher? {
+    return NoOptPatcher
   }
 
-  private val patcherDigest =
-    (dokiTheme.id + dokiTheme.version).toByteArray(Charsets.UTF_8)
+  override fun forPath(path: String?): SVGLoader.SvgElementColorPatcher? {
+    return NoOptPatcher
+  }
+}
 
-  override fun forPath(path: String?) =
-    buildHackedPatcher(
-      runSafelyWithResult({
-        otherColorPatcherProvider.forPath(path)
-      }) {
-        null
-      }
-    )
+object ColorPatcher : PatcherProvider {
+
+  private var otherColorPatcherProvider: PatcherProvider = noOptPatcherProvider
+  private var uiColorPatcherProvider: PatcherProvider = noOptPatcherProvider
+  private lateinit var dokiTheme: DokiTheme
+
+  private val patcherProviderCache = HashSet<String>()
+
+  override fun forPath(path: String?): Patcher {
+    val safeKey = path ?: "ayyLmao"
+    return if (patcherProviderCache.add(safeKey)) {
+      val buildHackedPatcher = buildHackedPatcher(
+        listOf(otherColorPatcherProvider, uiColorPatcherProvider)
+          .distinct()
+          .mapNotNull { patcherProvider ->
+            runSafelyWithResult({
+              patcherProvider.forPath(path)
+            }) {
+              null
+            }
+          },
+        safeKey
+      )
+      patcherProviderCache.remove(safeKey)
+      buildHackedPatcher
+    } else {
+      NoOptPatcher
+    }
+  }
 
   override fun forURL(url: URL?) =
     buildHackedPatcher(
-      runSafelyWithResult({
-        otherColorPatcherProvider.forURL(url)
-      }) {
-        null
-      }
+      listOf(otherColorPatcherProvider, uiColorPatcherProvider)
+        .distinct()
+        .mapNotNull { patcherProvider ->
+          runSafelyWithResult({
+            patcherProvider.forURL(url)
+          }) {
+            null
+          }
+        },
+      url?.toString() ?: "ayyLmao"
     )
+
+  private val patcherCache: Cache<String, Patcher> = CacheBuilder.newBuilder()
+    .expireAfterWrite(Duration.ofMinutes(1))
+    .build()
+
   private fun buildHackedPatcher(
-    otherPatcher: Patcher?
+    otherPatchers: List<Patcher>,
+    patcherKey: String,
   ): Patcher {
+    val ifPresent = patcherCache.getIfPresent(patcherKey)
+    if (ifPresent !== null) {
+      return ifPresent
+    }
 
     val self = this
-    return object : Patcher {
+    val value = object : Patcher {
+      private val svgCache = HashSet<Element>()
+
       override fun patchColors(svg: Element) {
-        self.patchColors(svg, otherPatcher)
+        if (svgCache.add(svg)) {
+          try {
+            runSafely({
+              self.patchColors(svg, otherPatchers)
+            })
+          } finally {
+            svgCache.remove(svg)
+          }
+        } else {
+          println("already patching bruv")
+        }
       }
 
       override fun digest(): ByteArray? {
         val shaDigest = DigestUtil.sha512()
-        shaDigest.update(otherPatcher?.digest() ?: emptyByteArray)
-        shaDigest.update(patcherDigest)
+        otherPatchers.forEach { otherPatcher ->
+          shaDigest.update(otherPatcher.digest() ?: emptyByteArray)
+        }
+        if (ColorPatcher::dokiTheme.isInitialized) {
+          shaDigest.update((dokiTheme.id + dokiTheme.version + "hax5").toByteArray(Charsets.UTF_8))
+        } else {
+          shaDigest.update(this.toString().toByteArray(Charsets.UTF_8))
+        }
         return shaDigest.digest()
       }
     }
+
+    patcherCache.put(patcherKey, value)
+
+    return value
   }
 
   fun patchColors(
     svg: Element,
-    otherPatcher: Patcher?
+    otherPatchers: List<Patcher>
   ) {
-    runSafely({
-      otherPatcher?.patchColors(svg)
-    })
+    otherPatchers.forEach { otherPatcher ->
+      runSafely({
+        otherPatcher?.patchColors(svg)
+      })
+    }
     patchChildren(
       svg,
-      otherPatcher
+      otherPatchers
     )
   }
 
-  private fun patchChildren(svg: Element, otherPatcher: Patcher?) {
+  private fun patchChildren(svg: Element, otherPatchers: List<Patcher>) {
     patchAccent(svg.getAttribute("accentTint"), svg) {
       it.toHexString()
     }
@@ -128,7 +195,7 @@ class ColorPatcher(
     for (i in 0 until length) {
       val item = nodes.item(i)
       if (item is Element) {
-        patchColors(item, otherPatcher)
+        patchColors(item, otherPatchers)
       }
     }
   }
@@ -157,4 +224,21 @@ class ColorPatcher(
 
   private fun getThemedStopColor() =
     namedColor("Doki.stopColor", Color.CYAN).toHexString()
+
+  fun setDokiTheme(dokiTheme: DokiTheme) {
+    this.dokiTheme = dokiTheme
+    LafManager.getInstance()
+      .currentLookAndFeel.toOptional()
+      .filter { it is UIThemeBasedLookAndFeelInfo }
+      .map { it as UIThemeBasedLookAndFeelInfo }
+      .ifPresent {
+        this.uiColorPatcherProvider = it.theme.colorPatcher
+      }
+    this.patcherCache.invalidateAll()
+  }
+
+  fun setOtherPatcher(otherPatcher: PatcherProvider) {
+    this.otherColorPatcherProvider = otherPatcher
+    this.patcherCache.invalidateAll()
+  }
 }
