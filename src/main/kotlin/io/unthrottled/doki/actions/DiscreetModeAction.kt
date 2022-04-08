56f4182ae94d1fcb31ea6b8b8dbc405505d61254
Alex Simons <alexsimons9999@gmail.com>
2022-04-08 05:56:30 -0500

    Enabling discreet mode from settings (fixes #503)

--- src/main/kotlin/io/unthrottled/doki/actions/DiscreetModeAction.kt
+++ src/main/kotlin/io/unthrottled/doki/actions/DiscreetModeAction.kt
@@ -1,18 +1,15 @@
 package io.unthrottled.doki.actions
 
 import com.intellij.openapi.actionSystem.AnActionEvent
-import com.intellij.openapi.application.ApplicationManager
 import io.unthrottled.doki.config.ThemeConfig
-import io.unthrottled.doki.discreet.DiscreetModeListener
 import io.unthrottled.doki.discreet.toDiscreetMode
+import io.unthrottled.doki.settings.actors.DiscreetModeActor
 
 class DiscreetModeAction : BaseToggleAction() {
   override fun isSelected(e: AnActionEvent): Boolean =
     ThemeConfig.instance.discreetMode
 
   override fun setSelected(e: AnActionEvent, state: Boolean) {
-    ApplicationManager.getApplication().messageBus
-      .syncPublisher(DiscreetModeListener.DISCREET_MODE_TOPIC)
-      .modeChanged(state.toDiscreetMode())
+    DiscreetModeActor.dispatchDiscreetMode(state.toDiscreetMode())
   }
 }
