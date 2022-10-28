52b976d2a1a77548abfed40f5ab0b21a3a53be71
Alex Simons <alexsimons9999@gmail.com>
2022-10-28 13:31:22 -0500

    Allowing Doki icons to be overridden.

--- src/main/java/icons/DokiThemeIcons.java
+++ src/main/java/icons/DokiThemeIcons.java
@@ -12,13 +12,9 @@ public class DokiThemeIcons {
   public static final Icon MAGIC = IconLoader.getIcon("/icons/doki/magic.svg", DokiThemeIcons.class);
   public static final Icon DIFF = IconLoader.getIcon("/icons/actions/diff.svg", DokiThemeIcons.class);
   public static final Icon TITLE_BAR = IconLoader.getIcon("/icons/doki/themedTitleBar.svg", DokiThemeIcons.class);
-  public static final Icon DDLC_FOLDER = IconLoader.getIcon("/icons/material/folders/ddlc.svg", DokiThemeIcons.class);
-  public static final Icon MATERIAL_FILES = IconLoader.getIcon("/icons/material/files/js.svg", DokiThemeIcons.class);
-  public static final Icon MATERIAL_GLYPH = IconLoader.getIcon("/icons/material/nodes/glyphs/class.svg", DokiThemeIcons.class);
   public static final Icon DOKI_LOGO = IconLoader.getIcon("/icons/doki/Doki-Doki-Logo.svg", DokiThemeIcons.class);
   public static final Icon AMII_LOGO = IconLoader.getIcon("/icons/plugins/amii/plugin-tool-window.svg", DokiThemeIcons.class);
-  // todo: figure out
-  public static final Icon WALLPAPER = IconLoader.getIcon("/icons/material/files/image.svg", DokiThemeIcons.class);
+  public static final Icon WALLPAPER = IconLoader.getIcon("/icons/doki/image.svg", DokiThemeIcons.class);
   public static final Icon GEAR_PLAIN = AllIcons.General.GearPlain;
   public static final Icon FAVORITES = AllIcons.Nodes.Favorite;
 }
