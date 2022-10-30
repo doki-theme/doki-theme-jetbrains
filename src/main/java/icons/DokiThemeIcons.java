1b7085dbee92913e1effbcb6a2890744c3412864
Alex Simons <alexsimons9999@gmail.com>
2022-10-30 09:57:45 -0500

    Potentially Fixing Icon Loading Issue.

---
+++ src/main/java/icons/DokiThemeIcons.java
@@ -0,0 +1,24 @@
+package icons;
+
+import com.intellij.icons.AllIcons;
+import com.intellij.openapi.util.IconLoader;
+
+import javax.swing.Icon;
+
+public class DokiThemeIcons {
+
+  public static final Icon CONFIG = IconLoader.getIcon("/icons/doki/pen.svg", DokiThemeIcons.class);
+  public static final Icon SHAME = IconLoader.getIcon("/icons/emojis/1f648.svg", DokiThemeIcons.class);
+  public static final Icon MAGIC = IconLoader.getIcon("/icons/doki/magic.svg", DokiThemeIcons.class);
+  public static final Icon DIFF = IconLoader.getIcon("/icons/actions/diff.svg", DokiThemeIcons.class);
+  public static final Icon TITLE_BAR = IconLoader.getIcon("/icons/doki/themedTitleBar.svg", DokiThemeIcons.class);
+  public static final Icon DDLC_FOLDER = IconLoader.getIcon("/icons/material/folders/ddlc.svg", DokiThemeIcons.class);
+  public static final Icon MATERIAL_FILES = IconLoader.getIcon("/icons/material/files/js.svg", DokiThemeIcons.class);
+  public static final Icon MATERIAL_GLYPH = IconLoader.getIcon("/icons/material/nodes/glyphs/class.svg", DokiThemeIcons.class);
+  public static final Icon DOKI_LOGO = IconLoader.getIcon("/icons/doki/Doki-Doki-Logo.svg", DokiThemeIcons.class);
+  public static final Icon AMII_LOGO = IconLoader.getIcon("/icons/plugins/amii/plugin-tool-window.svg", DokiThemeIcons.class);
+  // todo: figure out
+  public static final Icon WALLPAPER = IconLoader.getIcon("/icons/material/files/image.svg", DokiThemeIcons.class);
+  public static final Icon GEAR_PLAIN = AllIcons.General.GearPlain;
+  public static final Icon FAVORITES = AllIcons.Nodes.Favorite;
+}
