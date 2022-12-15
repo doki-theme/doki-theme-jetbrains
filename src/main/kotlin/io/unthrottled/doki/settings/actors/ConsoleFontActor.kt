6819afdacf58bca7fb72becd1ca5d10e21868392
Alex Simons <alexsimons9999@gmail.com>
2022-12-14 18:42:43 -0600

    Sneaky refresh.

--- src/main/kotlin/io/unthrottled/doki/settings/actors/ConsoleFontActor.kt
+++ src/main/kotlin/io/unthrottled/doki/settings/actors/ConsoleFontActor.kt
@@ -4,8 +4,11 @@ import com.intellij.openapi.editor.colors.EditorColorsManager
 import com.intellij.openapi.editor.colors.impl.EditorColorsManagerImpl
 import io.unthrottled.doki.config.ThemeConfig
 import io.unthrottled.doki.service.ConsoleFontService
+import io.unthrottled.doki.util.Logging
+import io.unthrottled.doki.util.logger
+import io.unthrottled.doki.util.runSafely
 
-object ConsoleFontActor {
+object ConsoleFontActor: Logging {
   fun enableCustomFontSize(enabled: Boolean, consoleFontName: String) {
     val previousEnablement = ThemeConfig.instance.isOverrideConsoleFont
     ThemeConfig.instance.isOverrideConsoleFont = enabled
@@ -21,6 +24,16 @@ object ConsoleFontActor {
   }
 
   fun refreshConsole() {
-    (EditorColorsManager.getInstance() as EditorColorsManagerImpl).schemeChangedOrSwitched(null)
+    runSafely({
+      val editorColorManager = EditorColorsManager.getInstance()
+      editorColorManager.javaClass.declaredMethods
+        .filter {
+          it.name == "schemeChangedOrSwitched" && it.canAccess(editorColorManager)
+        }.forEach {
+          it.invoke(editorColorManager, null)
+        }
+    }) {
+      logger().warn("Unable to refresh console", it)
+    }
   }
 }
