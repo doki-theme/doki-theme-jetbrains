592e68997c26b4072b3ec497167c9d8551a5e390
Alex Simons <alexsimons9999@gmail.com>
2023-10-01 21:31:04 -0500

    Updated Project startup interface.

--- src/main/kotlin/io/unthrottled/doki/listener/PluginPostStartUpActivity.kt
+++ src/main/kotlin/io/unthrottled/doki/listener/PluginPostStartUpActivity.kt
@@ -1,11 +1,20 @@
 package io.unthrottled.doki.listener
 
 import com.intellij.openapi.project.Project
+import com.intellij.openapi.startup.ProjectActivity
 import com.intellij.openapi.startup.StartupActivity
 import io.unthrottled.doki.TheDokiTheme
 
-class PluginPostStartUpActivity : StartupActivity {
+class PluginPostStartUpActivity : ProjectActivity, StartupActivity {
   override fun runActivity(project: Project) {
+    this.doStuff(project)
+  }
+
+  override suspend fun execute(project: Project) {
+    doStuff(project)
+  }
+
+  private fun doStuff(project: Project) {
     TheDokiTheme.instance.projectOpened(project)
   }
 }
